775fc511dfb24a1ef204d2374185fd6b36bfb055
Alvaro Ortega <alvaro.ortega@revenga.com>
2024-01-19 01:58:56 -0800

    #2237

---
+++ ritsback/resources/db/dml/maqueta_dgt_jaen/08_plans/02.conf.plan_nodes.sql
@@ -0,0 +1,13 @@
+INSERT INTO conf.plan_nodes (node_id, alias, description, plan_id, management_area_id, enabled, visible) VALUES
+(1, 'Apertura', 'Apertura', NULL, NULL, True, True),
+(2, 'Cierre', 'Cierre', NULL, NULL, True, True),
+(3, 'Precaución', 'Precaución', NULL, NULL, True, True),
+(4, 'ABRIR TÚNELES SENTIDO -> NORTE', 'ABRIR TÚNELES SENTIDO -> NORTE', 1, NULL, True, True),
+(5, 'ABRIR TÚNELES SENTIDO -> SUR', 'ABRIR TÚNELES SENTIDO -> SUR', 2, NULL, True, True),
+(6, 'CERRAR TÚNELES SENTIDO -> NORTE', 'CERRAR TÚNELES SENTIDO -> NORTE', 3, NULL, True, True),
+(7, 'CERRAR TÚNELES SENTIDO -> SUR', 'CERRAR TÚNELES SENTIDO -> SUR', 4, NULL, True, True),
+(8, 'PRECAUCIÓN TÚNELES SENTIDO -> NORTE', 'PRECAUCIÓN TÚNELES SENTIDO -> NORTE', 5, NULL, True, True),
+(9, 'PRECAUCIÓN TÚNELES SENTIDO -> SUR', 'PRECAUCIÓN TÚNELES SENTIDO -> SUR', 6, NULL, True, True),
+(10, 'CORTE CARRIL DERECHO -> NORTE', 'CORTE CARRIL DERECHO -> NORTE', 7, NULL, True, True),
+(11, 'CORTE CARRIL DERECHO -> SUR', 'CORTE CARRIL DERECHO -> SUR', 8, NULL, True, True);
+
--- ritsfront/resources/db/dml/maqueta_dgt_jaen/03.menus.sql
+++ ritsfront/resources/db/dml/maqueta_dgt_jaen/03.menus.sql
@@ -1,7 +1,8 @@
 INSERT INTO ui.menu_groups(id, "label", icon, "name", description, enabled, visible, position) VALUES
 (1, 'LBL_MENU_MAPS_AND_SYNOPTICS', 'img/menu/default.png', 'Menu Maps & Synoptics', 'Menu Maps & Synoptics', true, true, 1),
 (2, 'LBL_MENU_EQUIPMENT', 'img/menu/default.png', 'Equipment', 'Equipment', true, true, 2),
-(3, 'LBL_MENU_TOOLS', 'img/menu/default.png', 'Menu Tools', 'Menu Tools', true, true, 3);
+(3, 'LBL_MENU_TOOLS', 'img/menu/default.png', 'Menu Tools', 'Menu Tools', true, true, 3),
+(4, 'LBL_MENU_MANAGER', 'img/menu/default.png', 'Menu Manager', 'Menu Manager', true, true, 4);
 
 INSERT INTO ui.menu_items(id, menu_group_id, "position", module_id, "label", icon, "name", description, enabled, visible) VALUES
 -- MAPS & SYNOPTICS
@@ -14,5 +15,8 @@ INSERT INTO ui.menu_items(id, menu_group_id, "position", module_id, "label", ico
 
 -- TOOLS
 (0301, 3, 1, 403, 'LBL_MENU_ITEM_REPORTS', 'img/menu/report.png', 'Menu item Reports', 'Menu item Reports', true, true),
+
+--MANAGER
+(0401, 4, 2, 1024, 'LBL_MENU_ITEM_PLAN_MANAGER', 'img/menu/plan-menu.png', 'Menu item Plan Manager', 'Menu item Plan Manager', true, true),
 
 (500001, 5000, 3, 1047, 'LBL_MENU_HELP_CONTENTS', 'img/menu/help.png', 'Help contents', 'Menu item help contents', true, true);
