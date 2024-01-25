d86fc7f687e1832cffbde70d1dcf19096b041e30
Alvaro Ortega <alvaro.ortega@revenga.com>
2024-01-25 09:17:15 -0800

    #2073

--- ritsfront/resources/db/dml/maqueta_fullequip/03_vo_extensions/10.log_type/02.entities_values.sql
+++ ritsfront/resources/db/dml/maqueta_fullequip/03_vo_extensions/10.log_type/02.entities_values.sql
@@ -1,4 +1,5 @@
 INSERT INTO ui.vo_entity_values(vo_entity_id, vo_entity_field_id, value) VALUES
+('LogSubtype:33', 42, 'INFO'),
 ('LogSubtype:38', 42, 'INFO'),
 ('LogSubtype:39', 42, 'INFO'),
 ('LogSubtype:42', 42, 'ERROR'),
