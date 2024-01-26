aeb3419374b5a59df54f3bd34bcfc25eec3838e1
jcaballeroa <jcaballeroa@revenga.com>
2024-01-26 16:03:13 +0100

    Actualización ficheros soporte Jenkins

--- ritsback/resources/db/ddlrt/ddl_param.sh
+++ ritsback/resources/db/ddlrt/ddl_param.sh
@@ -3,5 +3,5 @@
 yourfilenames=`find . -name '*.sql' -print0 | sort -z | xargs -r0`
 for eachfile in $yourfilenames
 do
-	psql -v ON_ERROR_STOP=1 postgresql://rits:rits@$1:$2/rits -b -f $eachfile >/dev/null
+	psql postgresql://rits:rits@$1:$2/rits -b -f $eachfile >/dev/null
 done
---
+++ ritsback/resources/db/make_param_jenkins.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo Executing sql scripts to generate Environment... \(errors that occur are shown below\)
+cd ddl
+/bin/bash ./ddl_param.sh $psqlHost $psqlPort
+if [ $? -ne 0 ]; then
+    exit 1  # Sale del script con un código de salida diferente de 0
+fi
+
+cd ..
+cd dml/master
+/bin/bash ./dml_param.sh $psqlHost $psqlPort
+if [ $? -ne 0 ]; then
+    echo "Error ejecutando psql para $eachfile"
+    exit 1  # Sale del script con un código de salida diferente de 0
+fi
+    
+cd ..
+cd $project
+/bin/bash ./dml_param.sh $psqlHost $psqlPort
+if [ $? -ne 0 ]; then
+    echo "Error ejecutando psql para $eachfile"
+    exit 1  # Sale del script con un código de salida diferente de 0
+fi
+    
+cd ../..
+cd updates
+/bin/bash ./dml_param.sh $psqlHost $psqlPort
+if [ $? -ne 0 ]; then
+    echo "Error ejecutando psql para $eachfile"
+    exit 1  # Sale del script con un código de salida diferente de 0
+fi
+    
+	
