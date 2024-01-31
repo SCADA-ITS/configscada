6684b4f576a551a8b54b903bf94d6a46bcc88361
Jdiaz <jesus.diaz@revenga.com>
2024-01-31 15:52:01 +0100

    #2254

--- ritsfront/resources/db/dml/maqueta_dgt_jaen/03.menus.sql
+++ ritsfront/resources/db/dml/maqueta_dgt_jaen/03.menus.sql
@@ -11,12 +11,12 @@ INSERT INTO ui.menu_items(id, menu_group_id, "position", module_id, "label", ico
 -- EQUIPMENT
 (0201, 2, 1, 500, 'LBL_MENU_ITEM_EQUIPMENTS', 'img/menu/equipment-menu.png', 'Menu item Equipments', 'Menu item Equipments', true, true),
 (0202, 2, 2, 1005, 'LBL_MENU_ITEM_ALARM_MONITOR', 'img/menu/alarm-monitor-menu-2.png', 'Menu item alarm monitor', 'Menu item alarm monitor', true, true),
-(0203, 2, 3, 1026, 'LBL_MENU_ITEM_VMS_SIGNALING', 'img/menu/signaling-menu.png', 'Menu item VMS Signaling', 'Menu item VMS Signaling', true, true),
 
 -- TOOLS
 (0301, 3, 1, 403, 'LBL_MENU_ITEM_REPORTS', 'img/menu/report.png', 'Menu item Reports', 'Menu item Reports', true, true),
 
 --MANAGER
 (0401, 4, 2, 1024, 'LBL_MENU_ITEM_PLAN_MANAGER', 'img/menu/plan-menu.png', 'Menu item Plan Manager', 'Menu item Plan Manager', true, true),
+(0402, 4, 3, 1040, 'LBL_MENU_ITEM_VMS_SIGNALING', 'img/menu/signaling-menu.png', 'Menu item VMS Signaling', 'Menu item VMS Signaling', true, true),
 
 (500001, 5000, 3, 1047, 'LBL_MENU_HELP_CONTENTS', 'img/menu/help.png', 'Help contents', 'Menu item help contents', true, true);
