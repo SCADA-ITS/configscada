4c0021bc78a687d7a505cf022aee0e467bd112a5
jcaballeroa <jcaballeroa@revenga.com>
2024-02-12 17:57:10 +0100

    Task #2270

--- ritsfront/openits/static/component/graphicIcon/custom/bus_dali.js
+++ ritsfront/openits/static/component/graphicIcon/custom/bus_dali.js
@@ -5,23 +5,23 @@ const ELEMENT_TYPE_ID = "ElementType:85";
 
 const PARAM_COMMAND_VALUE_ID_G_DATA_JSON = "ElementTypeParam:85:2:2";
 
-const ELEMENT_TYPE_STATES_NO_TREATMENT = ["ElementTypeState:85:0","ElementTypeState:85:2"];
+const ELEMENT_TYPE_STATES_NO_TREATMENT = ["ElementTypeState:85:0", "ElementTypeState:85:2"];
 
 const G_ID_TEXT_VALUE_P = "textValueP";
 const G_ID_TEXT_VALUE_R = "textValueR";
 
 export default class BusDali extends GraphicIconCustomType {
-	
+
 	constructor(s, g, elementInfo, events) {
 
 		super(s, g, elementInfo, events);
-		
+
 		this.gState = this.g.select("#" + GraphicIcon.G_ID_STATE);
 
 		this.elementTypeStatesNoTreatment = ELEMENT_TYPE_STATES_NO_TREATMENT;
-	
+
 		this.elementTypeId = ELEMENT_TYPE_ID;
-		
+
 		this.gTextValueP = this.g.select("#" + G_ID_TEXT_VALUE_P);
 		this.gTextValueR = this.g.select("#" + G_ID_TEXT_VALUE_R);
 	}
@@ -38,18 +38,18 @@ export default class BusDali extends GraphicIconCustomType {
 			let treatmentValue = (ELEMENT_TYPE_STATES_NO_TREATMENT.indexOf(elementInfo.state.id) === -1);
 
 			let cmdGroupValue = this.getValue(PARAM_COMMAND_VALUE_ID_G_DATA_JSON);
-			
+
 			if (cmdGroupValue && treatmentValue) {
 				var data = JSON.parse(cmdGroupValue);
-				for (let groups of data.groups) {
-					  if(groups.id === 0)
-					  	  	this.gTextValueP.attr("text", groups.value.toString()  + "%");
-						else if (groups.id === 1)	
+				if (data.groups !== undefined) {
+					for (let groups of data.groups) {
+						if (groups.id === 0)
+							this.gTextValueP.attr("text", groups.value.toString() + "%");
+						else if (groups.id === 1)
 							this.gTextValueR.attr("text", groups.value.toString() + "%");
 					}
-				
+				}
 			}
-		
 		}
 	}
 }
