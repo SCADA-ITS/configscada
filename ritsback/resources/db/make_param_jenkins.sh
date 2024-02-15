251d804025160c3203234629d5825acfb6ce6b7e
jcaballeroa <jcaballeroa@revenga.com>
2024-02-15 15:43:00 +0100

    Actualización ficheros soporte Jenkins

--- ritsback/resources/db/make_param_jenkins.sh
+++ ritsback/resources/db/make_param_jenkins.sh
@@ -6,14 +6,14 @@ psqlPort="$3"
 
 echo Executing sql scripts to generate Environment... \(errors that occur are shown below\)
 cd ddl
-/bin/bash ./ddl_param.sh $psqlHost $psqlPort
+/bin/bash ./ddl_param_jenkins.sh $psqlHost $psqlPort
 if [ $? -ne 0 ]; then
     exit 1  # Sale del script con un código de salida diferente de 0
 fi
 
 cd ..
 cd dml/master
-/bin/bash ./dml_param.sh $psqlHost $psqlPort
+/bin/bash ./dml_param_jenkins.sh $psqlHost $psqlPort
 if [ $? -ne 0 ]; then
     echo "Error ejecutando psql para $eachfile"
     exit 1  # Sale del script con un código de salida diferente de 0
@@ -21,7 +21,7 @@ fi
     
 cd ..
 cd $project
-/bin/bash ./dml_param.sh $psqlHost $psqlPort
+/bin/bash ./dml_param_jenkins.sh $psqlHost $psqlPort
 if [ $? -ne 0 ]; then
     echo "Error ejecutando psql para $eachfile"
     exit 1  # Sale del script con un código de salida diferente de 0
@@ -29,7 +29,7 @@ fi
     
 cd ../..
 cd updates
-/bin/bash ./dml_param.sh $psqlHost $psqlPort
+/bin/bash ./dml_param_jenkins.sh $psqlHost $psqlPort
 if [ $? -ne 0 ]; then
     echo "Error ejecutando psql para $eachfile"
     exit 1  # Sale del script con un código de salida diferente de 0
