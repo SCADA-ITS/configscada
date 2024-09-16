172b722a2f7731b0daaad148820b465bf267df1b
Alvaro Ortega <alvaro.ortega@revenga.com>
2024-09-16 18:03:40 +0200

    #2599

--- startup/Selenium/tests/test_03_jasper.py
+++ startup/Selenium/tests/test_03_jasper.py
@@ -1,24 +1,28 @@
-import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 def test_jasper(firefox_browser):
-    # 3 | Esperar hasta que el elemento "Herramientas" esté visible y clicarlo
+    # Esperar hasta que el elemento "Herramientas" esté visible y clicarlo
     WebDriverWait(firefox_browser, 20).until(
         EC.visibility_of_element_located((By.LINK_TEXT, "Herramientas"))
     ).click()
 
-    # 4 | Esperar y hacer clic en el submenú
+    # Esperar y hacer clic en el submenú
     WebDriverWait(firefox_browser, 20).until(
         EC.element_to_be_clickable((By.CLASS_NAME, "submenu-text"))
     ).click()
 
-    # 5 | Esperar hasta que el iframe esté presente y cargado
-    WebDriverWait(firefox_browser, 20).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, "iframe[src*='/jasperserver']"))
+    # Esperar hasta que el iframe esté presente y cargado
+    iframe = WebDriverWait(firefox_browser, 20).until(
+        EC.presence_of_element_located((By.XPATH, '//*[contains(@src, "jasperserver")]'))
     )
 
-    # Opcional: agregar una espera adicional para asegurar que el contenido del iframe se cargue completamente.
-    time.sleep(2)
+    # Cambiamos el contexto para ubicarnos en el frame
+    firefox_browser.switch_to.frame(iframe)
+
+    # Esperamos a que cargue la lista de resultados de jasperserver
+    WebDriverWait(firefox_browser, 20).until(
+        EC.presence_of_element_located((By.ID, "resultsList"))
+    )
