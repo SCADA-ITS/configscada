c8deac6e2299779f0b9d6e98b9da4afb7bc5bc6e
Alvaro Ortega <alvaro.ortega@revenga.com>
2024-09-17 12:06:08 +0200

    #2609

--- startup/Selenium/tests/test_01_alarmas.py
+++ startup/Selenium/tests/test_01_alarmas.py
@@ -1,13 +1,20 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
+from datetime import datetime
 import time
-import pytest
+import csv
 
 
 
 def test_alarmas(firefox_browser):
+
+    
+    # datetime object containing current date and time
+    now = datetime.now()
+    # dd/mm/YY H:M:S
+    date = now.strftime("%d-%m-%Y_%H-%M-%S")
+
     # Espero a que el menú "Equipamiento" cuyo id es webix_l_id="2" sea visible y clico
     WebDriverWait(firefox_browser, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "[webix_l_id='2']"))).click()
     # Clico la opción "Monitor de alarmas" cuyo id es webix_l_id="2.202"
@@ -17,66 +24,41 @@ def test_alarmas(firefox_browser):
     WebDriverWait(firefox_browser, 10).until(EC.presence_of_element_located((By.XPATH, '//*[contains(@view_id, "viewDtNoRecognized")]')))
     WebDriverWait(firefox_browser, 10).until(EC.presence_of_element_located((By.XPATH, '//*[contains(@view_id, "viewDtRecognized")]')))
 
-    errores = []
+    alarmas_no_reconocidas(firefox_browser, date)
+    alarmas_reconocidas(firefox_browser, date)
 
-    try:
-        res1 = alarmas_no_reconocidas(firefox_browser)
-        if not res1:
-            errores.append("Se han encontrado alarmas graves no reconocidas")
-    except Exception as e:
-        print(f"Error capturado en la primera función: {e}")
-
-    try:
-        res2 = alarmas_reconocidas(firefox_browser)
-        if not res2:
-            errores.append("Se han encontrado alarmas graves reconocidas")
-    except Exception as e:
-        print(f"Error capturado en la segunda función: {e}")
-
-    # Si hay errores, marcar el test como fallido con todos los mensajes de error
-    if errores:
-        pytest.fail("\n".join(errores))
-
-
-def alarmas_no_reconocidas(firefox_browser):
+def alarmas_no_reconocidas(firefox_browser, date):
+    alarmas_no_reconocidas_fecha =[]
     alarmas_no_reconocidas_equipos =[]
     alarmas_no_reconocidas_nombre = []
+    alarmas_no_reconocidas_nivel = []
+
     # Identifico el contenedor de la tabla de alarmas reconocidas
     not_recognized_alarms_main = firefox_browser.find_element(By.XPATH, '//*[contains(@view_id, "viewDtNoRecognized")]')
-    # Hago click en el botón de filtro
-    not_recognized_alarms_main.find_element(By.XPATH, './div[2]/div/div[contains(@view_id, "tb_btn-showfilters")]').click()
-    # Hago clic en el select de nivel de alrma
-    not_recognized_alarms_main.find_element(By.XPATH, './div[3]/div[1]/div[2]/table/tbody/tr[3]/td[7]/div/select').click()
-    # Hago clic en la opción "Nivel 4"
-    filtro_nivel_4 = not_recognized_alarms_main.find_element(By.XPATH, './div[3]/div[1]/div[2]/table/tbody/tr[3]/td[7]/div/select/option[contains(@value, "Nivel 4")]')
-
-    if not filtro_nivel_4:
-        print("No existen alarmas no reconocidas de nivel 4")
-        return
-    else:
-        filtro_nivel_4.click()
-
-    # Se ordena por nombre
-    not_recognized_alarms_main.find_element(By.XPATH, './div[3]/div[1]/div[2]/table/tbody/tr[2]/td[6]/div/div[1]').click()
 
     #Se guarda en una variable la datatable
     not_recognized_datatable = not_recognized_alarms_main.find_element(By.XPATH, './div[3]')
     total_elementos = int(not_recognized_datatable.get_dom_attribute("aria-rowcount"))
-    print(f"Existen un total de {total_elementos} alarmas sin reconocer")
+    print(f"\nExisten un total de {total_elementos} alarmas sin reconocer")
 
     scroll_container = not_recognized_datatable.find_element(By.CSS_SELECTOR, '.webix_ss_vscroll.webix_vscroll_y')
     while True:
         # Se busca a traves de la datatable las alarmas y los equipos
+        columna_fecha = not_recognized_datatable.find_elements(By.XPATH, './div[2]/div[2]/div[1]/div[3]/div/span')
         columna_alarmas = not_recognized_datatable.find_elements(By.XPATH, './div[2]/div[2]/div[1]/div[4]/div/span')
         columna_equipos = not_recognized_datatable.find_elements(By.XPATH, './div[2]/div[2]/div[1]/div[6]/div/span')
+        columna_nivel = not_recognized_datatable.find_elements(By.XPATH, './div[2]/div[2]/div[1]/div[7]/div/img')        
 
         # Imprimir las listas de alarmas y equipos
-        for alarma, equipo in zip(columna_alarmas, columna_equipos):
+        for fecha, alarma, equipo, nivel in zip(columna_fecha, columna_alarmas, columna_equipos, columna_nivel):
             if(alarma.text == "" or equipo.text == ""):
                 pass
             else:
+                alarmas_no_reconocidas_fecha.append(fecha.text)
                 alarmas_no_reconocidas_equipos.append(equipo.text)
                 alarmas_no_reconocidas_nombre.append(alarma.text)
+                alarmas_no_reconocidas_nivel.append(nivel.get_dom_attribute("src")[23:-4])
+                
 
         scroll_height = firefox_browser.execute_script("return arguments[0].scrollHeight", scroll_container)
         scroll_top = firefox_browser.execute_script("return arguments[0].scrollTop", scroll_container)
@@ -87,62 +69,53 @@ def alarmas_no_reconocidas(firefox_browser):
             break
 
         firefox_browser.execute_script("arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;", scroll_container)
-        time.sleep(1)
+        time.sleep(0.5)
 
-    alarmas_no_reconocidas = list(set(list(zip(alarmas_no_reconocidas_equipos, alarmas_no_reconocidas_nombre))))
-    with open("Alarmas_no_reconocidas.txt", "w") as file:
-        for equipo, alarma in alarmas_no_reconocidas:
-            file.write(f"Equipo: {equipo}, Alarma: {alarma} \n")
+    alarmas_no_reconocidas = list(set(list(zip(alarmas_no_reconocidas_fecha, alarmas_no_reconocidas_nombre, alarmas_no_reconocidas_equipos, alarmas_no_reconocidas_nivel))))
+    
+    columnas_csv = ["fecha", "alarma", "equipo", "nivel"]
+    with open(f'alarmas_no_reconocidas-{date}.csv', mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(columnas_csv)
+        for alarma in alarmas_no_reconocidas:
+            writer.writerow(alarma)
         file.close()
 
     assert len(alarmas_no_reconocidas) == total_elementos
 
-    if len(alarmas_no_reconocidas) > 0:
-        print("Se han encontrado alarmas graves reconocidas")
-        return False
-    else:
-        return True
 
-
-def alarmas_reconocidas(firefox_browser):
+def alarmas_reconocidas(firefox_browser, date):
+    alarmas_reconocidas_fecha =[]
     alarmas_reconocidas_equipos =[]
     alarmas_reconocidas_nombre = []
+    alarmas_reconocidas_nivel =[]
 
     # Identifico el contenedor de la tabla de alarmas reconocidas
     recognized_alarms_main = firefox_browser.find_element(By.XPATH, '//*[contains(@view_id, "viewDtRecognized")]')
-    # Hago click en el botón de filtro
-    recognized_alarms_main.find_element(By.XPATH, './div[2]/div/div[contains(@view_id, "tb_btn-showfilters")]').click()
-    # Hago clic en el select de nivel de alrma
-    recognized_alarms_main.find_element(By.XPATH, './div[3]/div[1]/div[2]/table/tbody/tr[3]/td[7]/div/select').click()
-    # Hago clic en la opción "Nivel 4"
-    filtro_nivel_4 = recognized_alarms_main.find_element(By.XPATH, './div[3]/div[1]/div[2]/table/tbody/tr[3]/td[7]/div/select/option[contains(@value, "Nivel 4")]')
-    if not filtro_nivel_4:
-        print("No existen alarmas reconocidas de nivel 4")
-        return
-    else:
-        filtro_nivel_4.click()
-
 
     #Se guarda en una variable la datatable
     recognized_datatable = recognized_alarms_main.find_element(By.XPATH, './div[3]')
     total_elementos = int(recognized_datatable.get_dom_attribute("aria-rowcount"))
-    print("")
     print(f"Existen un total de {total_elementos} alarmas reconocidas")
 
     scroll_container = recognized_datatable.find_element(By.CSS_SELECTOR, '.webix_ss_vscroll.webix_vscroll_y')
     while True:
         # Se busca a traves de la datatable las alarmas y los equipos
+        recognized_columna_fecha = recognized_datatable.find_elements(By.XPATH, './div[2]/div[2]/div[1]/div[3]/div/span')
         recognized_columna_alarmas = recognized_datatable.find_elements(By.XPATH, './div[2]/div[2]/div[1]/div[4]/div/span')
         recognized_columna_equipos = recognized_datatable.find_elements(By.XPATH, './div[2]/div[2]/div[1]/div[6]/div/span')
+        recognized_columna_nivel = recognized_datatable.find_elements(By.XPATH, './div[2]/div[2]/div[1]/div[7]/div/img')
 
 
         # Imprimir las listas de alarmas y equipos
-        for alarma, equipo in zip(recognized_columna_alarmas, recognized_columna_equipos):
+        for fecha, alarma, equipo, nivel in zip(recognized_columna_fecha, recognized_columna_alarmas, recognized_columna_equipos, recognized_columna_nivel):
             if(alarma.text == "" or equipo.text == ""):
                 pass
             else:
+                alarmas_reconocidas_fecha.append(fecha.text)
                 alarmas_reconocidas_equipos.append(equipo.text)
                 alarmas_reconocidas_nombre.append(alarma.text)
+                alarmas_reconocidas_nivel.append(nivel.get_dom_attribute("src")[23:-4])
         
         scroll_height = firefox_browser.execute_script("return arguments[0].scrollHeight", scroll_container)
         scroll_top = firefox_browser.execute_script("return arguments[0].scrollTop", scroll_container)
@@ -156,17 +129,14 @@ def alarmas_reconocidas(firefox_browser):
         time.sleep(0.5)
 
     
-    alarmas_reconocidas = list(set(list(zip(alarmas_reconocidas_equipos, alarmas_reconocidas_nombre))))
-
-    with open("Alarmas_reconocidas.txt", "w") as file:
-        for equipo, alarma in alarmas_reconocidas:
-            file.write(f"Equipo: {equipo}, Alarma: {alarma} \n")
+    alarmas_reconocidas = list(set(list(zip(alarmas_reconocidas_fecha, alarmas_reconocidas_nombre, alarmas_reconocidas_equipos, alarmas_reconocidas_nivel))))
+    
+    columnas_csv = ["fecha", "alarma", "equipo", "nivel"]
+    with open(f'alarmas_reconocidas-{date}.csv', mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(columnas_csv)
+        for alarma in alarmas_reconocidas:
+            writer.writerow(alarma)
         file.close()
 
     assert len(alarmas_reconocidas) == total_elementos
-
-    if len(alarmas_reconocidas) > 0:
-        print("Se han encontrado alarmas graves reconocidas")
-        return False
-    else:
-        return True
