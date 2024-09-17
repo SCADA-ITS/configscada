46aecd8384c7456c1844435d6aef5365f1bdfe0b
Jdiaz <jesus.diaz@revenga.com>
2024-09-17 16:31:48 +0200

    #2607

---
+++ ritsfront/resources/db/dml/openits_plus/07_equip_viewers/58.PULSADOR.sql
@@ -0,0 +1,14 @@
+INSERT INTO ui.equip_viewers (id, name, description, "label", icon, show_info_tab, show_childs, show_actions, initial_tab, measure_width, measure_max_rows, measure_only, graphic_icon_id, info_tab_mode, no_show_properties) VALUES
+('ElementType:58','PULSADOR','PULSADOR','LBL_ELEMENT_TYPE_PULSADOR',null,True,False,false,null,225.0,3,null,58,2,true);
+
+INSERT INTO ui.equip_viewer_tabs (equip_viewer_id, module_action_id, alias, description, "label", icon, "position", visible) VALUES
+('ElementType:58',100501,'Alarms module' ,'Alarms module', 'LBL_ALARMS', null,  1, True );
+
+--equip_viewer_grp_measure_id = Subtype(2d) + Type(4d) + contador
+--if it is going to be put by element use this format for the id: ELEMENT + Subtype(2d) + Type(4d) + contador
+INSERT INTO ui.equip_viewer_grp_measures(id, equip_viewer_id, name, description, "label", icon, position, visible) VALUES
+(00005801, 'ElementType:58', 'Measures', 'Measures', 'LBL_GRP_MEASURES', null, 1, True);
+
+INSERT INTO ui.equip_viewer_configs (id, equip_viewer_id, vo_entities_id, "position", visible) VALUES
+(005801,'ElementType:58','ElementTypeParam:58:1:1', 1, True),
+(005802,'ElementType:58','ElementTypeParam:58:1:2', 2, True);
