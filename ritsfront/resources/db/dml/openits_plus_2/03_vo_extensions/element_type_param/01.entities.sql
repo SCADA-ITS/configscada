3f088989b0d6a74f17abad3f526a7f4e1063f01f
Jdiaz <jesus.diaz@revenga.com>
2024-09-18 10:12:14 +0200

    #2607

--- ritsfront/resources/db/dml/openits_plus_2/03_vo_extensions/element_type_param/01.entities.sql
+++ ritsfront/resources/db/dml/openits_plus_2/03_vo_extensions/element_type_param/01.entities.sql
@@ -2,6 +2,9 @@ INSERT INTO ui.vo_entities(id, vo_entity_type_id, enabled, visible) VALUES
 ('ElementTypeParam:58:1:1', 'ElementTypeParamUI', true, true),
 ('ElementTypeParam:58:1:2', 'ElementTypeParamUI', true, true),
 
+('ElementTypeParam:59:1:1', 'ElementTypeParamUI', true, true),
+('ElementTypeParam:59:1:2', 'ElementTypeParamUI', true, true),
+
 ('ElementTypeParam:89:1:1', 'ElementTypeParamUI', true, true),
 ('ElementTypeParam:89:1:2', 'ElementTypeParamUI', true, true);
 
