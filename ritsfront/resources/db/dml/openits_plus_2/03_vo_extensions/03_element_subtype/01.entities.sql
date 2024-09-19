0c0a53166c71312e13480cf70c3462d2abc290b1
jgarin <joaquin.garin@revenga.com>
2024-09-19 15:52:47 +0200

    #2605

---
+++ ritsfront/resources/db/dml/openits_plus_2/03_vo_extensions/03_element_subtype/01.entities.sql
@@ -0,0 +1,2 @@
+INSERT INTO ui.vo_entities(id, vo_entity_type_id, enabled, visible) VALUES
+('ElementSubtype:1:11', 'ElementSubtypeUI', true, true);
--- ritsfront/resources/db/dml/openits_plus_2/03_vo_extensions/element_type_param/01.entities.sql
+++ ritsfront/resources/db/dml/openits_plus_2/03_vo_extensions/element_type_param/01.entities.sql
@@ -1,4 +1,7 @@
 INSERT INTO ui.vo_entities(id, vo_entity_type_id, enabled, visible) VALUES
+('ElementTypeParam:11:1:1', 'ElementTypeParamUI', true, true),
+('ElementTypeParam:11:1:2', 'ElementTypeParamUI', true, true),
+
 ('ElementTypeParam:58:1:1', 'ElementTypeParamUI', true, true),
 ('ElementTypeParam:58:1:2', 'ElementTypeParamUI', true, true),
 
@@ -10,7 +13,5 @@ INSERT INTO ui.vo_entities(id, vo_entity_type_id, enabled, visible) VALUES
 ('ElementTypeParam:84:1:5', 'ElementTypeParamUI', true, true),
 ('ElementTypeParam:84:2:1', 'ElementTypeParamUI', true, true),
 
-
 ('ElementTypeParam:89:1:1', 'ElementTypeParamUI', true, true),
 ('ElementTypeParam:89:1:2', 'ElementTypeParamUI', true, true);
-
