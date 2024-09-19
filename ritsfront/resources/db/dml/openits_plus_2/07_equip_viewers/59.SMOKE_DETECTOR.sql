d11e420e6ea075d214ef48fc144347584fce1a6b
Jdiaz <jesus.diaz@revenga.com>
2024-09-19 09:24:53 +0200

    #2607

--- ritsfront/resources/db/dml/openits_plus_2/07_equip_viewers/59.SMOKE_DETECTOR.sql
+++ ritsfront/resources/db/dml/openits_plus_2/07_equip_viewers/59.SMOKE_DETECTOR.sql
@@ -1,5 +1,5 @@
 INSERT INTO ui.equip_viewers (id, name, description, "label", icon, show_info_tab, show_childs, show_actions, initial_tab, measure_width, measure_max_rows, measure_only, graphic_icon_id, info_tab_mode, no_show_properties) VALUES
-('ElementType:59','SMOKE_DETECTOR','SMOKE_DETECTOR','LBL_ELEMENT_TYPE_SMOKE_DETECTOR',null,True,False,True,null,225.0,3,null,59,2,null);
+('ElementType:59','SMOKE_DETECTOR','SMOKE_DETECTOR','LBL_ELEMENT_TYPE_SMOKE_DETECTOR',null,True,False,false,null,225.0,3,null,59,2,true);
 
 INSERT INTO ui.equip_viewer_tabs (equip_viewer_id, module_action_id, alias, description, "label", icon, "position", visible) VALUES
 ('ElementType:59',100501,'Alarms module' ,'Alarms module', 'LBL_ALARMS', null,  1, True );
