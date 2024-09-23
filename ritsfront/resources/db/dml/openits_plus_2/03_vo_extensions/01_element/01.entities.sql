f64c4b9e85c63846851d8ddd811a59afabde7bc3
jcaballeroa <jcaballeroa@revenga.com>
2024-09-23 11:15:32 +0200

    Task #2331

---
+++ ritsfront/resources/db/dml/openits_plus_2/03_vo_extensions/01_element/01.entities.sql
@@ -0,0 +1,11 @@
+INSERT INTO ui.vo_entities(id, vo_entity_type_id, enabled, visible) VALUES
+('Element:2001:1', 'ElementUI', true, true),
+('Element:2001:2', 'ElementUI', true, true),
+
+('Element:2002:1', 'ElementUI', true, true),
+('Element:2002:2', 'ElementUI', true, true);
+
+
+
+
+
