7f525203563b400d5a56a61ff02fadd99df6049a
Alvaro Ortega <alvaro.ortega@revenga.com>
2024-09-30 11:43:04 +0200

    #2606

--- ritsfront/resources/db/dml/openits_plus_2/03_vo_extensions/02_element_type/02.entities_values.sql
+++ ritsfront/resources/db/dml/openits_plus_2/03_vo_extensions/02_element_type/02.entities_values.sql
@@ -173,6 +173,7 @@ INSERT INTO ui.vo_entity_values(vo_entity_id, vo_entity_field_id, value) VALUES
 ('ElementType:71', 2, 'graphic_icon/71_vms_dgt.svg'),
 ('ElementType:71', 14, 'grid/element_type_71.png'),
 ('ElementType:71', 23, '102601'),
+('ElementType:71', 30, '102602'),
 
 ('ElementType:74', 1, '74'),
 ('ElementType:74', 2, 'graphic_icon/74_eru.svg'),
