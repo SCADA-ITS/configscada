b92006ba1a0158a2bd3157f0ea18b512ce4a3feb
Jdiaz <jesus.diaz@revenga.com>
2024-10-01 10:46:52 +0200

    #2618

--- ritsback/resources/db/dml/openits_plus_2/02_roads/04.stretchs.sql
+++ ritsback/resources/db/dml/openits_plus_2/02_roads/04.stretchs.sql
@@ -7,6 +7,11 @@ INSERT INTO conf.stretchs (stretch_id, stretch_type_id, alias, description, pk_i
 (1005, 2, 'Central', 'Central', 0, 0, 0, 4, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
 (1006, 2, 'Tunel Sur', 'Túnel Sur', 0, 0, 0, 2, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
 (1007, 2, 'Tunel Norte', 'Túnel Norte', 0, 0, 0, 2, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(1008, 2, 'Tunel Sur SEC 1', 'Tunel Sur SEC 1', 0, 0, 0, 2, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(1009, 2, 'Tunel Sur SEC 2', 'Tunel Sur SEC 2', 0, 0, 0, 2, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(1010, 2, 'Tunel Norte SEC 1', 'Tunel Norte SEC 1', 0, 0, 0, 2, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(1011, 2, 'Tunel Norte SEC 2', 'Tunel Norte SEC 2', 0, 0, 0, 2, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+
 
 --INCIDENCIAS
 (4001, 2, 'Tunel Sur', 'Tubo sentido Sur', 0, 0, 0, 1, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
