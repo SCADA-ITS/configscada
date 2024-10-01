20d5cb397604947f2f98ec0c7b76ee22d1b4dfc4
Alvaro Ortega <alvaro.ortega@revenga.com>
2024-10-01 12:04:58 +0200

    #2606

--- ritsback/resources/db/dml/openits_plus_2/10_vms_graphics/02_conf/01_vms_graphic_groups/19.gr_txt/01.vms_graphic_groups.sql
+++ ritsback/resources/db/dml/openits_plus_2/10_vms_graphics/02_conf/01_vms_graphic_groups/19.gr_txt/01.vms_graphic_groups.sql
@@ -4,7 +4,19 @@ INSERT INTO conf.vms_graphic_groups (graphic_group_id, alias, description, label
 
 INSERT INTO conf.vms_graphic_groups_elements (graphic_group_id, element_type_id, element_id, enabled, visible, created_at, updated_at) VALUES
 (19, 71, 1, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-(19, 71, 2, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+(19, 71, 2, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(19, 71, 3, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(19, 71, 4, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(19, 71, 5, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(19, 71, 6, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(19, 71, 7, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(19, 71, 8, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(19, 71, 9, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(19, 71, 10, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(19, 71, 11, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(19, 71, 12, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(19, 71, 13, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(19, 71, 14, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 INSERT INTO conf.vms_graphic_graphic_group_values (graphic_type_id, graphic_id, graphic_group_id, value, enabled, visible, created_at, updated_at) VALUES
 (1, 8000, 19, '1' ,true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
