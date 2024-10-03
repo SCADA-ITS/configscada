fa879fee770ca8ec717f3ee49c94cae0180240f0
jcaballeroa <jcaballeroa@revenga.com>
2024-10-03 12:34:08 +0200

    Task #2608

--- ritsback/resources/db/dml/openits_plus_2/05_alarms/01_alarm_measures/100.botonera.sql
+++ ritsback/resources/db/dml/openits_plus_2/05_alarms/01_alarm_measures/100.botonera.sql
@@ -12,6 +12,6 @@ INSERT INTO conf.alarm_configs
 --
 INSERT INTO conf.alarm_config_measures
 (alarm_config_id, element_type_id, param_type_id, element_type_param_id, alarm_threshold_operation_id, threshold_val1, threshold_val2, created_at, updated_at) VALUES
-(03010100, 100, 2, 1, 7, '20', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-(03020100, 100, 2, 2, 7, '20', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-(03030100, 100, 2, 3, 7, '20', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+(03010100, 100, 2, 1, 5, '20', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(03020100, 100, 2, 2, 5, '20', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(03030100, 100, 2, 3, 5, '20', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
