d9f3945e34ce6c273c49afa1312c4d025f7f5253
jcaballeroa <jcaballeroa@revenga.com>
2024-10-14 14:02:56 +0200

    Task #2679

--- resources/project/oferta_colombia/start.sh
+++ resources/project/oferta_colombia/start.sh
@@ -5,7 +5,7 @@ pkill --signal SIGKILL -f frontrits
 
 
 echo Loading SERVER-LAUNCHER-MANAGER
-nohup java -jar /home/revenga/app/backrits/server-launcher.jar  -Dfile.encoding=UTF-8 --xml.adapter=server_launcher_manager_adapter_v1.xml --xml.server_launcher_adapter=server_launcher_adapter_v1.xml &
+nohup java -jar /home/admin/app/backrits/server-launcher.jar  -Dfile.encoding=UTF-8 --xml.adapter=server_launcher_manager_adapter_v1.xml --xml.server_launcher_adapter=server_launcher_adapter_v1.xml &
 sleep 5
 echo Loading FRONT
-nohup java -Dspring.profiles.active=oferta_colombia -jar /home/revenga/app/frontrits/openits.jar
+nohup java -Dspring.profiles.active=oferta_colombia -jar /home/admin/app/frontrits/openits.jar
