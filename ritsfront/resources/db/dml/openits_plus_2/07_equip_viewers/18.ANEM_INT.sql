16a54b3dd241bf146843c55c98302203ae3d601c
Alvaro Ortega <alvaro.ortega@revenga.com>
2024-10-31 15:33:20 +0100

    #2616 #2605

--- ritsfront/resources/db/dml/openits_plus_2/07_equip_viewers/18.ANEM_INT.sql
+++ ritsfront/resources/db/dml/openits_plus_2/07_equip_viewers/18.ANEM_INT.sql
@@ -1,5 +1,5 @@
 INSERT INTO ui.equip_viewers (id, name, description, "label", icon, show_info_tab, show_childs, show_actions, initial_tab, measure_width, measure_max_rows, measure_only, graphic_icon_id, info_tab_mode, no_show_properties,show_historical_chart) VALUES
-('ElementType:18','ANEM_INT','ANEM_INT','LBL_ELEMENT_TYPE_ANEM_INT',null,True,False,True,null,225.0,3,null,18,2,True,true);
+('ElementType:18','ANEM_INT','ANEM_INT','LBL_ELEMENT_TYPE_ANEM_INT',null,True,False,false,null,225.0,3,null,18,2,True,true);
 
 INSERT INTO ui.equip_viewer_tabs (equip_viewer_id, module_action_id, alias, description, "label", icon, "position", visible) VALUES
 ('ElementType:18',100501,'Alarms module' ,'Alarms module', 'LBL_ALARMS', null,  1, True );
--- ritsfront/resources/db/dml/openits_plus_2/07_equip_viewers/8.BARRIER.sql
+++ ritsfront/resources/db/dml/openits_plus_2/07_equip_viewers/8.BARRIER.sql
@@ -1,5 +1,5 @@
 INSERT INTO ui.equip_viewers (id, name, description, "label", icon, show_info_tab, show_childs, show_actions, initial_tab, measure_width, measure_max_rows, measure_only, graphic_icon_id, info_tab_mode, no_show_properties) VALUES
-('ElementType:8','Barrier AUS','Barrier AUS','LBL_ELEMENT_SUBTYPE_BARRIER_AUS',null,True,False,True,null,225.0,3,null,8,2,null);
+('ElementType:8','Barrier AUS','Barrier AUS','LBL_ELEMENT_SUBTYPE_BARRIER_AUS',null,True,False,True,null,225.0,3,null,8,2,true);
 
 INSERT INTO ui.equip_viewer_tabs (equip_viewer_id, module_action_id, alias, description, "label", icon, "position", visible) VALUES
 ('ElementType:8',100501,'Alarms module' ,'Alarms module', 'LBL_ALARMS', null, 1, True);
@@ -12,7 +12,3 @@ INSERT INTO ui.equip_viewer_grp_measures(id, equip_viewer_id, name, description,
 INSERT INTO ui.equip_viewer_measures (equip_viewer_grp_measure_id, vo_entities_id, position, visible) VALUES
 --Barrier AUS
 (02000801,'ElementTypeParam:8:2:1', 1, True);
-
-INSERT INTO ui.equip_viewer_configs (id, equip_viewer_id, vo_entities_id, "position", visible) VALUES
-(080201,'ElementType:8','ElementTypeParam:8:1:1', 1, True),
-(080202,'ElementType:8','ElementTypeParam:8:1:2', 2, True);
