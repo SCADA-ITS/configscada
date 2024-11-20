39953e5ebdcd41929a6fff10d2e0a441e5a4d52f
revenga <revenga@revenga-VMware-Virtual-Platform>
2024-11-20 16:16:11 +0100

    Task #2725 #2726

--- ritsback/resources/db/dml/navarra/03_elements/02.camara.sql
+++ ritsback/resources/db/dml/navarra/03_elements/02.camara.sql
@@ -70,7 +70,7 @@ INSERT INTO conf.element_values (element_type_id, element_type_param_id, param_t
 
 (2, 1, 1, 10, NULL, 'Hikvision', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), 
 (2, 2, 1, 10, NULL, 'DS-2CD3047G2-LS', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), 
-(2, 3, 1, 10, NULL, 'https://descontroltrafico.admon-cfnavarra.es/api/stream.mjpeg?src=Berriozar', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), 
+(2, 3, 1, 10, NULL, 'https://descontroltrafico.admon-cfnavarra.es/rtc/api/stream.mjpeg?src=Berriozar', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), 
 (2, 6, 1, 10, NULL, '[{"id": 1,"alias": "Preset 1","description": "Preset 1","default": true},{"id": 2,"alias": "Preset 2","description": "Preset 2","default": false},{"id": 3,"alias": "Preset 3","description": "Preset 3","default": false},{"id": 4,"alias": "Preset 4","description": "Preset 4": false},{"id": 5,"alias": "Preset 5","description": "Preset 5","default": false},{"id": 6,"alias": "Preset 6","description": "Preset 6","default": false}]', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), 
 (2, 7, 1, 10, NULL, 'PTZ', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), 
 (2, 9, 1, 10, NULL, '10.191.10.11', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), 
@@ -106,7 +106,7 @@ INSERT INTO conf.element_values (element_type_id, element_type_param_id, param_t
 
 (2, 1, 1, 16, NULL, 'Bosch', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), 
 (2, 2, 1, 16, NULL, 'Starlight-7000', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), 
-(2, 3, 1, 16, NULL, 'https://descontroltrafico.admon-cfnavarra.es/api/stream.mjpeg?src=Ezkaba', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), 
+(2, 3, 1, 16, NULL, 'https://descontroltrafico.admon-cfnavarra.es/rtc/api/stream.mjpeg?src=Ezkaba', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), 
 (2, 1003, 1, 16, NULL, 'Ezkaba', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), 
 
 (2, 1, 1, 17, NULL, 'Hikvision', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), 
@@ -126,7 +126,7 @@ INSERT INTO conf.element_values (element_type_id, element_type_param_id, param_t
 
 (2, 1, 1, 20, NULL, 'Axis', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), 
 (2, 2, 1, 20, NULL, 'P1375-E', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), 
-(2, 3, 1, 20, NULL, 'https://descontroltrafico.admon-cfnavarra.es/api/stream.mjpeg?src=Endarlatza', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), 
+(2, 3, 1, 20, NULL, 'https://descontroltrafico.admon-cfnavarra.es/rtc/api/stream.mjpeg?src=Endarlatza', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), 
 (2, 1003, 1, 20, NULL, 'Endarlatza', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 INSERT INTO conf.element_io_controller_modules (element_type_id, element_id, module_type_id, module_id, enabled, visible, created_at, updated_at) VALUES
--- ritsfront/resources/db/dml/maqueta_navarra/09_selector_trees/01.selector_trees_backoffice.sql
+++ ritsfront/resources/db/dml/maqueta_navarra/09_selector_trees/01.selector_trees_backoffice.sql
@@ -2,6 +2,6 @@ INSERT INTO ui.selector_trees(id, "label", default_module_id, enabled, visible) 
 (1, 'LBL_SELECTOR_TREE_EXTERNAL_INCIDENTS', 1950, true, true);
 
 INSERT INTO ui.selector_tree_nodes(id, selector_tree_id, "label", image, module_id, enabled, visible) VALUES
-(2, 1, 'LBL_EXT_ENTITY_C4', null, 1950, true, true);
+(2, 1, 'LBL_EXT_ENTITY_C4', 'ext_entity_type/ext_entity_C4', 1950, true, true);
 
 
