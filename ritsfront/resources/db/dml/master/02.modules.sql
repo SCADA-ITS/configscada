f1912731e923ab732d56c61741d436c4b46b8de0
juanpjimenez <juanp.jimenez@revenga.com>
2024-12-12 17:34:45 +0100

    Update #2760.

--- ritsfront/resources/db/dml/master/02.modules.sql
+++ ritsfront/resources/db/dml/master/02.modules.sql
@@ -236,6 +236,7 @@ INSERT INTO ui.module_actions(id, module_id, name, description, system_permissio
 (105002, 1050, 'edit', 'edit location', null, null),
 (105003, 1050, 'delete', 'delete location', null, null),
 (105004, 1050, 'select', 'select location', null, null),
+(105005, 1050, 'delete from Incident Wizard', 'delete from Incident Wizard', null, null),
 (105201, 1052, 'edit', 'edit delayed report', null, null),
 (105202, 1052, 'enable', 'enable delayed report', null, null),
 (105203, 1052, 'disable', 'disable delayed report', null, null),
