790e5aeeb74e99fe76189fde53c57823af6abf43
Jdiaz <jesus.diaz@revenga.com>
2024-12-13 13:50:58 +0100

    #2764

--- ritsback/resources/db/dml/maqueta_dgt_jaen/02_io_controller/01.io_controller.sql
+++ ritsback/resources/db/dml/maqueta_dgt_jaen/02_io_controller/01.io_controller.sql
@@ -6,16 +6,17 @@ DELETE FROM conf.io_controller_modules;
 -- Table: conf.io_controller_modules
 --
 INSERT INTO conf.io_controller_modules
-(module_type_id, module_id, alias, description, enabled, visible, created_at, updated_at)
-VALUES(1, 1, 'MANGO', 'MANGO', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+(module_type_id, module_id, alias, description, enabled, visible, created_at, updated_at)VALUES
+(1, 1, 'MANGO', 'MANGO', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(4, 1, ' METEO_SOCKET_UNE', 'METEO_SOCKET_UNE', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 
 -- 
 -- Table: conf.io_controller_module_values
 --
 INSERT INTO conf.io_controller_module_values
-(module_type_id, module_id, module_type_param_id, value, enabled, visible, created_at, updated_at)
-VALUES(1, 1, 1, 'true', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(module_type_id, module_id, module_type_param_id, value, enabled, visible, created_at, updated_at)VALUES
+(1, 1, 1, 'true', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
 (1, 1, 2, null, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
 (1, 1, 3, 'true', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
 (1, 1, 4, null, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
@@ -27,8 +28,8 @@ VALUES(1, 1, 1, 'true', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
 (1, 1, 10, '/api/v2/generic', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
 (1, 1, 11, '3000', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
 (1, 1, 12, '40', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-(1, 1, 13, 'http://192.168.88.52:8080/rest/v1', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-(1, 1, 14, 'http://192.168.88.52:8080/rest/v2/login', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(1, 1, 13, 'http://192.168.88.203:8080/rest/v1', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(1, 1, 14, 'http://192.168.88.203:8080/rest/v2/login', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
 (1, 1, 15, 'admin', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
 (1, 1, 16, 'DES_4942', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
 (1, 1, 17, '/point-values/{xids}/latest-multiple-points-single-array?limit=1&unitConversion=false&useCache=true&useRendered=false', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
@@ -42,4 +43,18 @@ VALUES(1, 1, 1, 'true', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
 (1, 1, 29, '/users/current', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
 (1, 1, 30, 'GET', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
 (1, 1, 31, '/data-points', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-(1, 1, 32, 'GET', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+(1, 1, 32, 'GET', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+
+(4, 1, 1, 'ElementTypeParam:1:1:4001', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(4, 1, 2, 'ElementTypeParam:1:1:4002', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(4, 1, 3, 'ElementTypeParam:1:1:4003', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(4, 1, 4, 'ElementTypeParam:1:1:4004', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(4, 1, 5, 'ElementTypeParam:1:1:4005', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(4, 1, 6, '0x10 0x20 0x30', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(4, 1, 7, '0/30 * * ? * * *', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(4, 1, 8, '1048576', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), -- 1024 * 124  1 mb
+(4, 1, 9, '180000', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(4, 1, 10, 'CRC_CCITT', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(4, 1, 11, '50', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(4, 1, 12, '500', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
--- ritsback/resources/db/dml/master/05_elements/01_element_types/01_meteo/02_element_subtype/05.Meteo_DGT_Jaen.sql
+++ ritsback/resources/db/dml/master/05_elements/01_element_types/01_meteo/02_element_subtype/05.Meteo_DGT_Jaen.sql
@@ -4,7 +4,7 @@
 --
 INSERT INTO master.element_subtypes
 (element_subtype_id, element_type_id, alias, description,label_alias, label_description, enabled, visible, created_at, updated_at)VALUES
-(5, 1, 'Meteo socket une', 'Meteo socket une', 'LBL_ELEMENT_SUBTYPE_METEO_SOCKET_UNE', 'LBL_ELEMENT_SUBTYPE_METEO_OCKET_UNE_DESC', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+(5, 1, 'Meteo socket une', 'Meteo socket une', 'LBL_ELEMENT_SUBTYPE_METEO_SOCKET_UNE', 'LBL_ELEMENT_SUBTYPE_METEO_SOCKET_UNE_DESC', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 -- 
 -- Table: master.element_subtype_active_params 
