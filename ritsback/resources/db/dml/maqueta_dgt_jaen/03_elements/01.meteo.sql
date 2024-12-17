4dc435b09defeb325dc5eb2c19d870bd510cef99
Jdiaz <jesus.diaz@revenga.com>
2024-12-17 16:32:50 +0100

    #2764

--- ritsback/resources/db/dml/maqueta_dgt_jaen/03_elements/01.meteo.sql
+++ ritsback/resources/db/dml/maqueta_dgt_jaen/03_elements/01.meteo.sql
@@ -9,11 +9,11 @@ INSERT INTO conf.element_values (element_type_id, element_type_param_id, param_t
 (1, 4004, 1, 1, NULL, '0x06', true, true, CURRENT_TIMESTAMP,CURRENT_TIMESTAMP), 
 (1, 4005, 1, 1, NULL, '0x20', true, true, CURRENT_TIMESTAMP,CURRENT_TIMESTAMP), 
 
-(1, 4001, 1, 2, NULL, '192.168.88.196', true, true, CURRENT_TIMESTAMP,CURRENT_TIMESTAMP), 
+(1, 4001, 1, 2, NULL, '192.168.88.197', true, true, CURRENT_TIMESTAMP,CURRENT_TIMESTAMP), 
 (1, 4002, 1, 2, NULL, '5000', true, true, CURRENT_TIMESTAMP,CURRENT_TIMESTAMP), 
 (1, 4003, 1, 2, NULL, '0x05 0x20', true, true, CURRENT_TIMESTAMP,CURRENT_TIMESTAMP), 
 (1, 4004, 1, 2, NULL, '0x06', true, true, CURRENT_TIMESTAMP,CURRENT_TIMESTAMP), 
-(1, 4005, 1, 2, NULL, '0x21', true, true, CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
+(1, 4005, 1, 2, NULL, '0x20', true, true, CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 
 
 INSERT INTO conf.element_io_controller_modules (element_type_id, element_id, module_type_id, module_id, enabled, visible, created_at, updated_at) VALUES
