1799a0d5547c8cd596dd2ea3b8b143e71be6ee83
Alvaro Ortega <alvaro.ortega@revenga.com>
2025-01-15 13:46:40 +0100

    #2844

--- ritsback/resources/db/dml/maqueta_dgt_jaen/06_system/01.system.sql
+++ ritsback/resources/db/dml/maqueta_dgt_jaen/06_system/01.system.sql
@@ -15,4 +15,6 @@ INSERT INTO conf.system_param_values
 (5, '1', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
 (6, '40', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
 (8, '0 0 * ? * * *', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-(14, '50', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+--Para el número máximo de alarmas sin reconocer --> n_elements = 1068, se deja a n_elements * 2 = 2136
+(14, '2136', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
