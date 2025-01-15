933467937d8f987a599e366181c7eef88beb2d40
Alvaro Ortega <alvaro.ortega@revenga.com>
2025-01-15 11:40:53 +0100

    #2333 Fix graphicIcon custom

---
+++ ritsfront/openits/project/dgt_jaen/component/graphicIcon/custom/pump_animation_alarmed.js
@@ -0,0 +1,72 @@
+import GraphicIconCustomType from "../graphicIconCustomType.js";
+import GraphicIcon from "../graphicIcon.js";
+
+const ELEMENT_TYPE_STATES_NO_TREATMENT = ["ElementTypeState:86:0","ElementTypeState:86:2"];
+const ELEMENT_TYPE_STATES_ALARMED = "ElementTypeState:86:3";
+
+const G_ID_ARROW_UP = "arrowUp";
+const G_ID_ARROW_DOWN = "arrowDown";
+const G_ID_FAN = "fan";
+const G_ID_ANIMATION = "animation";
+
+
+
+const SPEED_FAN = 1.3; // From 1 to 10. Lower value is more slow
+const MAX_TIME_ROTATE = 10;
+const FAN_START_POSITION = "0 29.3 28.5";
+const FAN_END_POSITION = "360 29.3 28.5";
+
+
+export default class Pump extends GraphicIconCustomType {
+	
+	constructor(s, g, elementInfo, events) {
+
+		super(s, g, elementInfo, events);
+		
+		this.gState = this.g.select("#" + GraphicIcon.G_ID_STATE);
+		this.elementTypeStatesNoTreatment = ELEMENT_TYPE_STATES_NO_TREATMENT;
+		
+		this.gArrowUp = this.g.select("#" + G_ID_ARROW_UP);
+        this.gArrowDown = this.g.select("#" + G_ID_ARROW_DOWN);
+
+        this.gFan = this.g.select("#" + G_ID_FAN)
+        this.gAnimation = this.g.select("#" + G_ID_ANIMATION);
+
+        
+	}
+	
+	render(elementInfo) {
+	       
+		this.elementInfo = elementInfo;
+
+		// Update state
+		if (this.gState) {
+
+            let timeFanRotate = MAX_TIME_ROTATE + 1 - SPEED_FAN + "s";
+
+
+			this.gState.attr({ fill: elementInfo.state.color });
+			
+			let treatmentValue = (ELEMENT_TYPE_STATES_NO_TREATMENT.indexOf(elementInfo.state.id) === -1);
+			console.log(elementInfo.state.id)
+
+            if (treatmentValue) {
+
+				
+				if(elementInfo.state.id == ELEMENT_TYPE_STATES_ALARMED){
+					this.gArrowUp.attr({ "fill-opacity": 1 });
+                    this.gArrowDown.attr({ "fill-opacity": 1 });
+                    this.gAnimation.attr({ dur: timeFanRotate, from: FAN_START_POSITION, to: FAN_END_POSITION });
+
+				}else {
+					this.gArrowUp.attr({ "fill-opacity": 0 });
+                    this.gArrowDown.attr({ "fill-opacity": 0 });
+                    this.gAnimation.attr({ dur: timeFanRotate, from: FAN_START_POSITION, to: FAN_START_POSITION });
+
+				}
+			}
+
+		}
+		
+	}
+}
---
+++ ritsfront/openits/project/dgt_jaen/component/graphicIcon/custom/tank_jaen.js
@@ -0,0 +1,76 @@
+import GraphicIconCustomType from "../graphicIconCustomType.js";
+import GraphicIcon from "../graphicIcon.js";
+
+const PARAM_COMMAND_VALUE_ID = "ElementTypeParam:87:2:4";
+
+const ELEMENT_TYPE_STATES_NO_TREATMENT = ["ElementTypeState:87:0", "ElementTypeState:87:2"];
+const ELEMENT_TYPE_STATES_UP = "ElementTypeState:87:1";
+const ELEMENT_TYPE_STATES_ALARM = "ElementTypeState:87:3";
+const G_ID_LEVEL_25 = "level25";
+const G_ID_LEVEL_50 = "level50";
+const G_ID_LEVEL_75 = "level75";
+const G_ID_LEVEL_100 = "level100";
+const G_ID_TEXT_VALUE = "textValue";
+
+export default class Tank extends GraphicIconCustomType {
+
+	constructor(s, g, elementInfo, events) {
+
+		super(s, g, elementInfo, events);
+
+		this.gState = this.g.select("#" + GraphicIcon.G_ID_STATE);
+
+		this.gLevel25 = this.g.select("#" + G_ID_LEVEL_25);
+		this.gLevel50 = this.g.select("#" + G_ID_LEVEL_50);
+		this.gLevel75 = this.g.select("#" + G_ID_LEVEL_75);
+		this.gLevel100 = this.g.select("#" + G_ID_LEVEL_100);	
+		this.gTextValue = this.g.select("#" + G_ID_TEXT_VALUE);	
+	}
+
+	render(elementInfo) {
+
+		this.elementInfo = elementInfo;
+
+		// Update state
+		if (this.gState) {
+
+			this.gState.attr({ fill: elementInfo.state.color });
+
+			let treatmentValue = (ELEMENT_TYPE_STATES_NO_TREATMENT.indexOf(elementInfo.state.id) === -1);
+
+			let cmdGroupValue = this.getValue(PARAM_COMMAND_VALUE_ID);
+			if (cmdGroupValue && treatmentValue) {
+				switch (true) {
+					case cmdGroupValue == 'true' && elementInfo.state.id == ELEMENT_TYPE_STATES_UP:
+						this.gLevel25.attr({ "fill-opacity": 1 });
+						this.gLevel50.attr({ "fill-opacity": 1 });
+						this.gLevel75.attr({ "fill-opacity": 1 });
+						this.gLevel100.attr({ "fill-opacity": 1 });
+						break;
+
+					case cmdGroupValue == 'false' && elementInfo.state.id == ELEMENT_TYPE_STATES_UP:
+						this.gLevel25.attr({ "fill-opacity": 1 });
+						this.gLevel50.attr({ "fill-opacity": 1 });
+						this.gLevel75.attr({ "fill-opacity": 0 });
+						this.gLevel100.attr({ "fill-opacity": 0 });
+						break;
+
+					case elementInfo.state.id == ELEMENT_TYPE_STATES_ALARM:
+						this.gLevel25.attr({ "fill-opacity": 1 });
+						this.gLevel50.attr({ "fill-opacity": 0 });
+						this.gLevel75.attr({ "fill-opacity": 0 });
+						this.gLevel100.attr({ "fill-opacity": 0 });
+						break;
+
+					default:
+						this.gLevel25.attr({ "fill-opacity": 0 });
+						this.gLevel50.attr({ "fill-opacity": 0 });
+						this.gLevel75.attr({ "fill-opacity": 0 });
+						this.gLevel100.attr({ "fill-opacity": 0 });
+						break;
+				}
+				this.gTextValue.remove()
+			}
+		}
+	}
+}
