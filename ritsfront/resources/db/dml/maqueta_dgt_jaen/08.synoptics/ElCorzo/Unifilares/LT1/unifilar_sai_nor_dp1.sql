0d536a0da479161e887f380f7e7fb2242ef0c088
Jdiaz <jesus.diaz@revenga.com>
2025-01-16 08:44:24 +0100

    #2548

---
+++ ritsfront/resources/db/dml/maqueta_dgt_jaen/08.synoptics/ElCorzo/Unifilares/LT1/unifilar_sai_nor_dp1.sql
@@ -0,0 +1,34 @@
+-- Para los identificadores de los sinopticos seguiremos los siguientes criterios
+-- Id iterado --> 3 cifras
+INSERT INTO ui.synoptics 
+(id, name, description, label, icon, base_layer, exclusive_layers) VALUES 
+(921, 'SELECTOR_TREE_NODES_SINOPTICO_UNIFILAR_SAI_NOR_DP1', 'SELECTOR_TREE_NODES_SINOPTICO_UNIFILAR_SAI_NOR_DP1', 'LBL_SELECTOR_TREE_NODES_SINOPTICO_UNIFILAR_SAI_NOR_DP1', NULL, 'img/synoptics/unifilar_sai_nor_dp1.svg', false);
+
+-- Para los identificadores de synoptic_layers seguiremos los siguientes criterios
+-- Id Synoptico                                   --> 3 cifras
+-- Id tipo (000 si no hay tipo asociado)          --> 3 cifras
+-- Id subTipo (00 si tiene tipo, iterador si no)  --> 2 cifras
+INSERT INTO ui.synoptic_layers 
+(id, synoptic_id, name, label, visible) VALUES 
+(92105700, 921, 'SYNOPTIC_LAYERS_INTERRUPTOR', 'LBL_SYNOPTIC_LAYERS_INTERRUPTOR', true);
+
+INSERT INTO ui.synoptic_commands 
+(id, synoptic_id, position, label, icon, "default", module_action_id, args, view_type_id, multiselect, show_text, item_required, context) VALUES 
+(60, 921, 0, 'LBL_CMD_ALARMS', 'mdi mdi-pencil', false, 100501, NULL, 2, true, NULL, false, true),
+(61, 921, 1, 'LBL_ELEMENT', 'mdi mdi-pencil', true, 101401, NULL, 2, false, NULL, true, true),
+(62, 921, 2, 'LBL_CMD_EQUIPMENT', 'mdi mdi-pencil', false, 101701, NULL, 2, true, NULL, false, true);
+
+INSERT INTO ui.synoptic_layer_elements 
+(layer_id, element_id, graphic_icon_id, label, x, y,  horizontal_flip, rotate, tooltip, show_text, text_color, selectable) VALUES 
+(92105700, 'Element:57:312', 00005701, 'INT-EMER-PERM-DER-SAI-NOR-1', 1817, 765, false, NULL, true, false, NULL, true),
+(92105700, 'Element:57:311', 00005701, 'INT-RED-FLUJO-SAI-NOR-1', 1816, 418, false, NULL, true, false, NULL, true),
+(92105700, 'Element:57:310', 00005701, 'INT-PMV-GA-SAI-NOR-1', 1569, 418, false, NULL, true, false, NULL, true),
+(92105700, 'Element:57:309', 00005701, 'INT-BAL-SAI-NOR-1', 1322, 418, false, NULL, true, false, NULL, true),
+(92105700, 'Element:57:308', 00005701, 'INT-VOD-METEO-SAI-NOR-1', 1074, 418, false, NULL, true, false, NULL, true),
+(92105700, 'Element:57:307', 00005701, 'INT-SOS-SAI-NOR-1', 826, 418, false, NULL, true, false, NULL, true),
+(92105700, 'Element:57:306', 00005701, 'INT-CIRC-1B-SAI-NOR-1', 571, 418, false, NULL, true, false, NULL, true),
+(92105700, 'Element:57:305', 00005701, 'INT-CIRC-1A-ERU-SAI-NOR-1', 327, 418, false, NULL, true, false, NULL, true),
+(92105700, 'Element:57:304', 00005701, 'INT-MANI-SAI-NOR-1', 84, 418, false, NULL, true, false, NULL, true),
+(92105700, 'Element:57:302', 00005701, 'INT-Q1-SAI-NOR-1', 951, 254, false, NULL, true, false, NULL, true),
+(92105700, 'Element:57:303', 00005703, 'INT-K01-SAI-NOR-1', 943, 317, false, NULL, true, false, NULL, true);
+
--- ritsfront/resources/db/dml/maqueta_dgt_jaen/12.selector_trees.sql
+++ ritsfront/resources/db/dml/maqueta_dgt_jaen/12.selector_trees.sql
@@ -33,7 +33,8 @@ INSERT INTO ui.selector_tree_nodes
 (27, 1, 'LBL_SELECTOR_TREE_NODES_SINOPTICO_DESPEÑAPERROS_TUNNEL_COMUNICACIONES', 917, 'menu/synoptic-menu.png', true, true),
 (28, 1, 'LBL_SELECTOR_TREE_NODES_SINOPTICO_CENTROS_TRANSFORMACION', 918, 'menu/synoptic-menu.png', true, true),
 (29, 1, 'LBL_SELECTOR_TREE_NODES_SINOPTICO_LA_CANTERA_TUNNEL_COMUNICACIONES', 919, 'menu/synoptic-menu.png', true, true),
-(30, 1, 'LBL_SELECTOR_TREE_NODES_SINOPTICO_EL_CORZO_TUNNEL_COMUNICACIONES', 920, 'menu/synoptic-menu.png', true, true);
+(30, 1, 'LBL_SELECTOR_TREE_NODES_SINOPTICO_EL_CORZO_TUNNEL_COMUNICACIONES', 920, 'menu/synoptic-menu.png', true, true),
+(31, 1, 'LBL_SELECTOR_TREE_NODES_SINOPTICO_UNIFILAR_SAI_NOR_DP1', 921, 'menu/synoptic-menu.png', true, true);
 
 INSERT INTO ui.selector_tree_hierarchies 
 (selector_tree_id, node_parent_id, node_child_id, enabled, visible) VALUES 
@@ -61,5 +62,6 @@ INSERT INTO ui.selector_tree_hierarchies
 (1, 23, 26, true, true),
 (1, 1, 27, true, true),
 (1, 4, 29, true, true),
-(1, 20, 30, true, true);
+(1, 20, 30, true, true),
+(1, 20, 31, true, true);
 
