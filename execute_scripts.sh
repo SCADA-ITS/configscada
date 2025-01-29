6362e5747d77cb84660ea1da3e47bf96ca2a8f58
jcaballeroa <jcaballeroa@revenga.com>
2025-01-29 13:54:25 +0100

    Task #2681

---
+++ execute_scripts.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Configuración
+REMOTE_USER="admin"
+REMOTE_HOST="192.168.88.160"
+SCADA_HOST="192.168.88.163"
+REMOTE_PATH="/home/admin/scripts"
+ARCHIVO_TAR="backup_sql.tar.gz"
+LOCAL_FOLDERS=("ritsback/resources/db" "ritsfront/resources/db")
+PROYECTO="maqueta_navarra"
+
+# Comprimir las carpetas antes de subirlas
+echo "Comprimiendo carpetas locales..."
+tar -czf $ARCHIVO_TAR "${LOCAL_FOLDERS[@]}"
+
+# Subir el archivo comprimido vía SFTP
+echo "Subiendo $ARCHIVO_TAR a $REMOTE_HOST..."
+sftp $REMOTE_USER@$REMOTE_HOST <<EOF
+put $ARCHIVO_TAR
+EOF
+
+# Conectar por SSH, descomprimir y ejecutar scripts SQL
+echo "Conectando a $REMOTE_HOST para descomprimir y ejecutar scripts..."
+ssh $REMOTE_USER@$REMOTE_HOST <<EOF
+mkdir -p $REMOTE_PATH
+mv $ARCHIVO_TAR $REMOTE_PATH/
+cd $REMOTE_PATH
+tar -xzf $ARCHIVO_TAR
+rm $ARCHIVO_TAR
+
+# Ejecutar los scripts SQL en el servidor remoto
+cd ritsback/resources/db
+./make_param.sh $PROYECTO $REMOTE_HOST 5430 2>/dev/null
+cd ../../../ritsfront/resources/db
+./make_param.sh $PROYECTO $REMOTE_HOST 5430 $SCADA_HOST 2>/dev/null
+EOF
+
+# Limpiar el archivo comprimido localmente
+rm $ARCHIVO_TAR
+
+echo "Proceso completado exitosamente."
