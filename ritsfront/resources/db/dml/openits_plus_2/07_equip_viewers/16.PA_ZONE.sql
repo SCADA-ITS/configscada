a9605a4c5dc43dc9ef70364d14fda39a05b3618b
jgarin <joaquin.garin@revenga.com>
2025-02-04 17:50:17 +0100

    #2731

--- ritsfront/resources/db/dml/openits_plus_2/07_equip_viewers/16.PA_ZONE.sql
+++ ritsfront/resources/db/dml/openits_plus_2/07_equip_viewers/16.PA_ZONE.sql
@@ -1,5 +1,5 @@
 INSERT INTO ui.equip_viewers (id, name, description, "label", icon, show_info_tab, show_childs, show_actions, initial_tab, measure_width, measure_max_rows, measure_only, graphic_icon_id, info_tab_mode, no_show_properties) VALUES
-('ElementType:16','PA_ZONE','PA_ZONE','LBL_ELEMENT_TYPE_PA_ZONE',null,True,False,True,null,225.0,3,null,16,2,True);
+('ElementType:16','PA_ZONE','PA_ZONE','LBL_ELEMENT_TYPE_PA_ZONE',null,True,False,False,null,225.0,3,null,16,2,True);
 
 INSERT INTO ui.equip_viewer_tabs (equip_viewer_id, module_action_id, alias, description, "label", icon, "position", visible) VALUES
 ('ElementType:16',100501,'Alarms module' ,'Alarms module', 'LBL_ALARMS', null,  1, True );
@@ -11,7 +11,7 @@ INSERT INTO ui.equip_viewer_grp_measures(id, equip_viewer_id, name, description,
 
 INSERT INTO ui.equip_viewer_measures (equip_viewer_grp_measure_id, vo_entities_id, position, visible) VALUES
 --PA_ZONE
-(00001601,'ElementTypeParam:16:2:1', 1, True),
+-- (00001601,'ElementTypeParam:16:2:1', 1, True),
 (00001601,'ElementTypeParam:16:2:1001', 1, True),
 (00001601,'ElementTypeParam:16:2:1002', 1, True),
 (00001601,'ElementTypeParam:16:2:1003', 1, True);
