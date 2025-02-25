fc818dd168083d74d8485f7812e8df81176d0319
Jdiaz <jesus.diaz@revenga.com>
2025-02-25 17:54:41 +0100

    #2686

--- ritsback/resources/db/dml/maqueta_dgt_jaen/11.state_machine/01_ventiliation/Despeñaperros/02.ventilación_confort.sql
+++ ritsback/resources/db/dml/maqueta_dgt_jaen/11.state_machine/01_ventiliation/Despeñaperros/02.ventilación_confort.sql
@@ -16,13 +16,13 @@ INSERT INTO static.sm_states
 INSERT INTO static.sm_thresholds
 (threshold_id, alias, description, label_alias, label_description, data_type_id, value, enabled, visible, created_at, updated_at) VALUES
 -- Estados
-(12, 'Threshold_CO_0', 'Threshold CO 0 (ppm)', 'LBL_THRESHOLD_CO_0', 'LBL_THRESHOLD_CO_0', 3, '15', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-(22, 'Threshold_CO_1', 'Threshold CO 1 (ppm)', 'LBL_THRESHOLD_CO_1', 'LBL_THRESHOLD_CO_1', 3, '30', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-(32, 'Threshold_CO_2', 'Threshold CO 2 (ppm)', 'LBL_THRESHOLD_CO_2', 'LBL_THRESHOLD_CO_2', 3, '100', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(1002, 'Threshold_CO_0', 'Threshold CO 0 (ppm)', 'LBL_THRESHOLD_CO_0', 'LBL_THRESHOLD_CO_0', 3, '15', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(2002, 'Threshold_CO_1', 'Threshold CO 1 (ppm)', 'LBL_THRESHOLD_CO_1', 'LBL_THRESHOLD_CO_1', 3, '30', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(3002, 'Threshold_CO_2', 'Threshold CO 2 (ppm)', 'LBL_THRESHOLD_CO_2', 'LBL_THRESHOLD_CO_2', 3, '100', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
 
-(42, 'Threshold_K_0', 'Threshold K 0 (k-1)', 'LBL_THRESHOLD_K_0', 'LBL_THRESHOLD_K_0', 3, '2.25', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-(52, 'Threshold_K_1', 'Threshold K 1 (k-1)', 'LBL_THRESHOLD_K_1', 'LBL_THRESHOLD_K_1', 3, '4.5', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-(62, 'Threshold_K_2', 'Threshold K 2 (k-1)', 'LBL_THRESHOLD_K_2', 'LBL_THRESHOLD_K_2', 3, '7.5', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+(4002, 'Threshold_K_0', 'Threshold K 0 (k-1)', 'LBL_THRESHOLD_K_0', 'LBL_THRESHOLD_K_0', 3, '2.25', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(5002, 'Threshold_K_1', 'Threshold K 1 (k-1)', 'LBL_THRESHOLD_K_1', 'LBL_THRESHOLD_K_1', 3, '4.5', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(6002, 'Threshold_K_2', 'Threshold K 2 (k-1)', 'LBL_THRESHOLD_K_2', 'LBL_THRESHOLD_K_2', 3, '7.5', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 
 INSERT INTO static.sm_transitions
@@ -49,63 +49,63 @@ INSERT INTO static.sm_transitions
 INSERT INTO static.sm_conditions
 (condition_id, threshold_operation_id, threshold_val1_id, threshold_val2_id, element_type_id, element_type_param_id, param_type_id, timer_id, enabled, visible, created_at, updated_at)VALUES
 -- Operations: 1 = Entre; 4 = Mayor que; 6 = Menor que
-(12, 6, 12, NULL, 19, 1, 2, NULL, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), --< 15 CO
-(22, 1, 12, 22, 19, 1, 2, NULL, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), --> 15 CO  < 30 CO 
-(32, 1, 22, 32, 19, 1, 2, NULL, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), -- > 30 CO < 100 CO 
-(42, 4, 32, NULL, 19, 1, 2, NULL, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), -- > 100 CO
+(1002, 6, 1002, NULL, 19, 1, 2, NULL, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), --< 15 CO
+(2002, 1, 1002, 2002, 19, 1, 2, NULL, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), --> 15 CO  < 30 CO 
+(3002, 1, 2002, 3002, 19, 1, 2, NULL, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), -- > 30 CO < 100 CO 
+(4002, 4, 3002, NULL, 19, 1, 2, NULL, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), -- > 100 CO
 
-(52, 6, 42, NULL, 21, 1, 2, NULL, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), --< 2.25 opac
-(62, 1, 42, 52, 21, 1, 2, NULL, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), -- > 2.25 opac < 4.5 opac
-(72, 1, 52, 62, 21, 1, 2, NULL, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), -- > 4.5 opac < 7.5 opac
-(82, 4, 62, NULL, 21, 1, 2, NULL, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP); -- > 7.5 opac
+(5002, 6, 4002, NULL, 21, 1, 2, NULL, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), --< 2.25 opac
+(6002, 1, 4002, 5002, 21, 1, 2, NULL, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), -- > 2.25 opac < 4.5 opac
+(7002, 1, 5002, 6002, 21, 1, 2, NULL, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), -- > 4.5 opac < 7.5 opac
+(8002, 4, 6002, NULL, 21, 1, 2, NULL, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP); -- > 7.5 opac
 
 
 INSERT INTO static.sm_transition_conditions
 (state_machine_id, state_id, transition_id, condition_id, enabled, visible, created_at, updated_at)VALUES
 --NORMAL -> ALTO
-(2, 2, 1, 22, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
-(2, 2, 1, 62, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
+(2, 2, 1, 2002, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
+(2, 2, 1, 6002, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
 
 --NORMAL -> ALTO-ALTO
-(2, 2, 2, 32, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
-(2, 2, 2, 72, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
+(2, 2, 2, 3002, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
+(2, 2, 2, 7002, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
 
 --NORMAL -> MAXIMO
-(2, 2, 3, 42, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
-(2, 2, 3, 82, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
+(2, 2, 3, 4002, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
+(2, 2, 3, 8002, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
 
 --ALTO -> NORMAL
-(2, 3, 3, 12, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
-(2, 3, 3, 52, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
+(2, 3, 3, 1002, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
+(2, 3, 3, 5002, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
 
 --ALTO -> ALTO-ALTO
-(2, 3, 1, 32, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
-(2, 3, 1, 72, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
+(2, 3, 1, 3002, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
+(2, 3, 1, 7002, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
 
 --ALTO -> MAXIMO
-(2, 3, 2, 42, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
-(2, 3, 2, 82, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
+(2, 3, 2, 4002, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
+(2, 3, 2, 8002, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
 
 --ALTO-ALTO -> NORMAL
-(2, 4, 3, 12, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
-(2, 4, 3, 52, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
+(2, 4, 3, 1002, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
+(2, 4, 3, 5002, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
 
 --ALTO-ALTO -> ALTO
-(2, 4, 2, 22, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
-(2, 4, 2, 62, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
+(2, 4, 2, 2002, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
+(2, 4, 2, 6002, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
 
 --ALTO-ALTO -> MAXIMO
-(2, 4, 1, 42, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
-(2, 4, 1, 82, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
+(2, 4, 1, 4002, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
+(2, 4, 1, 8002, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
 
 --MAXIMO -> NORMAL
-(2, 5, 3, 12, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
-(2, 5, 3, 52, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
+(2, 5, 3, 1002, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
+(2, 5, 3, 5002, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
 
 --MAXIMO -> ALTO
-(2, 5, 2, 22, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
-(2, 5, 2, 62, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
+(2, 5, 2, 2002, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
+(2, 5, 2, 6002, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),  
 
 --MAXIMO -> ALTO-ALTO
-(2, 5, 1, 32, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-(2, 5, 1, 72, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+(2, 5, 1, 3002, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(2, 5, 1, 7002, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
