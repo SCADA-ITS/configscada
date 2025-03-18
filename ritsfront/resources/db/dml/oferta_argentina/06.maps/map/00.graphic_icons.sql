d6b371d2a8aaefb9a2d08bbd1e4060a209a6b492
jcaballeroa <jcaballeroa@revenga.com>
2025-03-18 09:14:25 +0100

    Oferta Argentina

--- ritsfront/resources/db/dml/oferta_argentina/06.maps/map/00.graphic_icons.sql
+++ ritsfront/resources/db/dml/oferta_argentina/06.maps/map/00.graphic_icons.sql
@@ -1,2 +1,5 @@
 INSERT INTO ui.graphic_icons (id, "name", description, alarms, measures, js_draw_file, width, height, svg_pattern_var, svg_file, js_custom_type_render) VALUES
-(2, 'CAM', 'CAM', false, false, 'component/graphicIcon/graphicIconDefault.js', 30, 30, null, 'img/graphic_icon/02_camera.svg', null);
+(2, 'CAM', 'CAM', false, false, 'component/graphicIcon/graphicIconDefault.js', 30, 30, null, 'img/graphic_icon/02_camera.svg', null),
+(4, 'ETD', 'ETD', false, true, 'component/graphicIcon/graphicIconCustom.js', 100, 100, null, 'img/graphic_icon/04_section.svg', 'component/graphicIcon/custom/section.js'),
+(71, 'PMV', 'PMV', false, true, 'component/graphicIcon/graphicIconCustom.js', 300, 100, null, 'img/graphic_icon/71_vms_dgt_gr_txt.svg', 'component/graphicIcon/custom/vms.js');
+
