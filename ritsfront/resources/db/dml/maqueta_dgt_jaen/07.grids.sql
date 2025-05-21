39693f6f3215e3a89bd0b4ae34b96456e3ee6757
Jdiaz <jesus.diaz@revenga.com>
2025-05-21 11:14:33 +0200

    #2975

--- ritsfront/resources/db/dml/maqueta_dgt_jaen/07.grids.sql
+++ ritsfront/resources/db/dml/maqueta_dgt_jaen/07.grids.sql
@@ -264,7 +264,7 @@ INSERT INTO ui.grid_fields(id, grid_id, grid_field_data_type_id, "position", req
 (840004, 84, 1, 4, true, true, false, false, NULL, NULL, 'state', false, false, NULL, false, false, 30, NULL, true, true, NULL, NULL, NULL, NULL, NULL),
 (840005, 84, 1, 5, true, true, false, true, 'LBL_GRID_FIELD_STATE_LABEL', 'LBL_GRID_FIELD_STATE_LABEL', 'stateLabel', false, false, NULL, true, false, 150, NULL, true, true, NULL, NULL, NULL, NULL, NULL),
 (840006, 84, 1 ,6, true, false, false, false, 'LBL_ELEMENT_TYPE_PARAM_CIRCUIT_STATE', 'LBL_ELEMENT_TYPE_PARAM_CIRCUIT_STATE_DESC', 'ElementTypeParam:84:2:1', false, false, 'left', true, false, 200, NULL, true, true, NULL, NULL, NULL, NULL, 0),
-(840007, 84, 1 ,7, true, false, false, false, 'LBL_ELEMENT_TYPE_PARAM_DIMING_LEVEL', 'LBL_ELEMENT_TYPE_PARAM_DIMING_LEVEL_DESC', 'ElementTypeParam:84:2:2', false, false, 'left', true, false, 200, NULL, true, true, NULL, NULL, NULL, NULL, 0),
+(840007, 84, 2 ,7, true, false, false, false, 'LBL_ELEMENT_TYPE_PARAM_DIMING_LEVEL', 'LBL_ELEMENT_TYPE_PARAM_DIMING_LEVEL_DESC', 'ElementTypeParam:84:2:2', false, false, 'left', false, false, 200, NULL, true, true, NULL, NULL, NULL, NULL, 0),
 
 (860001, 86, 1, 1, true, false, false, false, 'LBL_GRID_FIELD_ID', 'LBL_GRID_FIELD_ID', 'id', true, false, null, false, false, 500, NULL, true, false, NULL, NULL, NULL, NULL, NULL),
 (860002, 86, 1, 2, true, false, false, true, 'LBL_GRID_FIELD_NAME', 'LBL_GRID_FIELD_NAME', 'alias', false, false, NULL, false, false, 170, NULL, true, true, NULL, NULL, true, NULL, NULL),
