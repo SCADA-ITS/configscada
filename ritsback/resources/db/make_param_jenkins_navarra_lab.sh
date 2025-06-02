9ebb674179b4c6c892a4970ae269a5b8d007b665
jcaballeroa <jcaballeroa@revenga.com>
2025-06-02 16:35:58 +0200

    Update Jenkins files to update project Navarra in lab machines - 2

--- ritsback/resources/db/make_param_jenkins_navarra_lab.sh
+++ ritsback/resources/db/make_param_jenkins_navarra_lab.sh
@@ -5,14 +5,14 @@ psqlHost="$2"
 psqlPort="$3"
 
 echo Executing sql scripts to generate Environment... \(errors that occur are shown below\)
-cd ddl
-/bin/bash ./ddl_param_jenkins_navarra_lab.sh $psqlHost $psqlPort
+cd ddl_navarra_lab
+/bin/bash ./ddl_param_jenkins.sh $psqlHost $psqlPort
 if [ $? -ne 0 ]; then
     exit 1  # Sale del script con un código de salida diferente de 0
 fi
 
 cd ..
-cd ddlrt
+cd ddlrt_navarra_lab
 /bin/bash ./ddl_param_jenkins_navarra_lab.sh $psqlHost $psqlPort
 if [ $? -ne 0 ]; then
     exit 1  # Sale del script con un código de salida diferente de 0
