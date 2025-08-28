d66225713800727808b1470ab834e2b8a91727a7
jcaballeroa <jcaballeroa@revenga.com>
2025-08-28 11:32:29 +0200

    Feature #3033

---
+++ resources/project/maqueta_p_industrial/equipment-manager/advisor/ChangeMeasureHistorical.groovy
@@ -0,0 +1,57 @@
+import java.util.List;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.apache.commons.lang3.tuple.Pair;
+
+import com.revenga.rits.back.equipment.manager.helper.GroovyHelper;
+import com.revenga.rits.back.data.core.model.Element;
+import com.revenga.rits.back.data.core.model.ElementValue;
+
+/**
+ *
+ * ChangeMeasureSample.groovy: Groovy for change measures of equipment
+ *
+ */
+class ChangeMeasureCamera {
+	
+	org.apache.logging.log4j.Logger log;
+	
+	ChangeMeasureCamera(org.apache.logging.log4j.Logger log) {
+
+		this.log = log;
+	}
+	
+	boolean process(Element element, List<Pair<ElementValue, ElementValue>> elementValues) {
+
+		List<ElementValue> listElementsValues;
+		try {
+
+			listElementsValues = new ArrayList();
+
+			if (!CollectionUtils.isEmpty(elementValues)) {
+				
+				for (Pair<ElementValue, ElementValue> pair : elementValues) {
+
+					ElementValue newElementValue = pair.getRight();
+					
+					listElementsValues.add(pair.getRight());
+				}
+				
+				element.setElementValues(listElementsValues);
+				
+				GroovyHelper.sendElementToHistoriclManager(element);
+
+			}
+			
+			return true;
+			
+		} catch (Exception e) {
+			
+			log.error(e.getMessage());
+			log.debug(ExceptionUtils.getStackTrace(e));
+		}
+		
+		return false;
+	}
+}
--- ritsback/resources/db/dml/p_industrial/03_elements/71.panel.sql
+++ ritsback/resources/db/dml/p_industrial/03_elements/71.panel.sql
@@ -1,6 +1,6 @@
 INSERT INTO conf.elements (element_type_id, element_subtype_id, element_id, location_id, keep_values_on_disconnected, inherit_state_id, alias, description, on_change_alarm, on_change_state, on_change_measure, on_schedule, enabled, visible, alert, alarm_count, created_at, updated_at) VALUES
-(71, 19, 1, 0010071, NULL, NULL, '01.4-ASC-PMV-01', '01.4-ASC-PMV-01', NULL, NULL, NULL, NULL, true, true, false, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), 
-(71, 19, 2, 0020071, NULL, NULL, '05.1-DES-PMV-01', '05.1-DES-PMV-01', NULL, NULL, NULL, NULL, true, true, false, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+(71, 19, 1, 0010071, NULL, NULL, '01.4-ASC-PMV-01', '01.4-ASC-PMV-01', NULL, NULL, 'ChangeMeasureHistorical.groovy', NULL, true, true, false, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), 
+(71, 19, 2, 0020071, NULL, NULL, '05.1-DES-PMV-01', '05.1-DES-PMV-01', NULL, NULL, 'ChangeMeasureHistorical.groovy', NULL, true, true, false, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 INSERT INTO conf.element_values (element_type_id, element_type_param_id, param_type_id, element_id, element_value_state_id, value, enabled, visible, created_at, updated_at) VALUES
 (71, 1, 1, 1, NULL, 'FIXALIA', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), 
