64be8b47194a732a91073e6f64cbc203d17d4d43
jcaballeroa <jcaballeroa@revenga.com>
2025-09-19 14:55:06 +0200

    Feature #3086

--- ritsback/resources/db/dml/maqueta_navarra/03_elements/90.sanctioning.sql
+++ ritsback/resources/db/dml/maqueta_navarra/03_elements/90.sanctioning.sql
@@ -2,7 +2,7 @@ INSERT INTO conf.elements (element_type_id, element_subtype_id, element_id, loca
 (90, NULL, 1, 0010099, NULL, NULL, 'SANCIONADOR', 'SANCIONADOR', NULL, NULL, NULL, NULL, true, true, false, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 INSERT INTO conf.element_values (element_type_id, element_type_param_id, param_type_id, element_id, element_value_state_id, value, enabled, visible, created_at, updated_at) VALUES
-(99, 1, 1, 1, NULL, 'false', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), 
-(99, 2, 1, 1, NULL, '3', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), 
-(99, 3, 1, 1, NULL, '99999999', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), 
-(99, 4, 1, 1, NULL, '6240899900001327607855', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+(90, 1, 1, 1, NULL, 'false', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), 
+(90, 2, 1, 1, NULL, '3', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), 
+(90, 3, 1, 1, NULL, '99999999', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), 
+(90, 4, 1, 1, NULL, '6240899900001327607855', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
