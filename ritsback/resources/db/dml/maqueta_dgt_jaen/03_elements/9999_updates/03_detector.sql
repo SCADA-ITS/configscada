0a7585187563439aa03a75e90efcabfe870e30f2
Jdiaz <jesus.diaz@revenga.com>
2025-10-17 10:02:50 +0200

    #3106

--- ritsback/resources/db/dml/maqueta_dgt_jaen/03_elements/9999_updates/03_detector.sql
+++ ritsback/resources/db/dml/maqueta_dgt_jaen/03_elements/9999_updates/03_detector.sql
@@ -1,5 +1,5 @@
 --ElementType:3
 UPDATE master.element_type_params 
 SET enabled = false
-WHERE element_type_id = 3 and param_type_id = 2 and element_type_param_id NOT IN (1,2,3,4,5,6,7,8,9,10,20,21,40,41,42);
+WHERE element_type_id = 3 and param_type_id = 2 and element_type_param_id NOT IN (1,2,3,4,5,6,7,8,9,10,13,17,20,21,40,41,42);
 
