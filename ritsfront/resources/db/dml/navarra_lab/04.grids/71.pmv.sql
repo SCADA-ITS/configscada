860bfd3ca98f91d496edd852ff2846efc353cdbe
jcaballeroa <jcaballeroa@revenga.com>
2025-11-06 15:52:53 +0100

    Feature #3011

--- ritsfront/resources/db/dml/navarra_lab/04.grids/71.pmv.sql
+++ ritsfront/resources/db/dml/navarra_lab/04.grids/71.pmv.sql
@@ -5,9 +5,7 @@ INSERT INTO ui.grid_fields(id, grid_id, grid_field_data_type_id, "position", req
 (010071, 71, 1, 1, true, false, false, false, 'LBL_ID', 'LBL_ID', 'id', true, false, null, false, false, 500, NULL, true, false, NULL, NULL, NULL, NULL),
 (020071, 71, 1, 2, true, false, false, true, 'LBL_NAME', 'LBL_NAME', 'alias', false, false, null, false, false, 150, NULL, true, true, NULL, NULL, NULL, NULL),
 (030071, 71, 1, 3, true, true, false, false, NULL, NULL, 'state', false, false, null, false, false, 35, NULL, true, true, NULL, NULL, NULL, NULL),
-(040071, 71, 1, 4, true, true, false, true, 'LBL_STATE_LABEL', 'LBL_STATE_LABEL', 'stateLabel', false, false, null, true, false, 150, NULL, true, true, NULL, NULL, NULL, NULL),
-(050071, 71, 1, 5, true, false, false, true, 'LBL_GRID_FIELD_BRAND', 'LBL_GRID_FIELD_BRAND', 'ElementTypeParam:2:1:1', false, false, 'left', false, false, 100, NULL, true, true, NULL, NULL, NULL, NULL),
-(060071, 71, 1, 6, true, false, false, true, 'LBL_GRID_FIELD_MODEL', 'LBL_GRID_FIELD_MODEL', 'ElementTypeParam:2:1:2', false, false, 'left', false, false, 200, NULL, true, true, NULL, NULL, NULL, NULL);
+(040071, 71, 1, 4, true, true, false, true, 'LBL_STATE_LABEL', 'LBL_STATE_LABEL', 'stateLabel', false, false, null, true, false, 150, NULL, true, true, NULL, NULL, NULL, NULL);
 
 INSERT INTO ui.grid_option_values(grid_id, grid_option_id, value) VALUES
 (71, 1, 'true'),
