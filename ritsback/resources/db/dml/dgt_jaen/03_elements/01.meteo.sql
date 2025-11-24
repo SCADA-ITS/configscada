63c5e026656bd2675ed92f5e2da2568150b7da55
Jdiaz <jesus.diaz@revenga.com>
2025-11-24 08:29:47 +0100

    #3138

--- ritsback/resources/db/dml/dgt_jaen/03_elements/01.meteo.sql
+++ ritsback/resources/db/dml/dgt_jaen/03_elements/01.meteo.sql
@@ -1,4 +1,4 @@
-INSERT INTO conf.elements (element_type_id, element_subtype_id, element_id, location_id, keep_values_on_disconnected, inherit_state_id, alias, description, on_change_alarm, on_change_state, on_change_measure, on_schedule, enabled, visible, alert, alarm_count, created_at, updated_at) VALUES
+operINSERT INTO conf.elements (element_type_id, element_subtype_id, element_id, location_id, keep_values_on_disconnected, inherit_state_id, alias, description, on_change_alarm, on_change_state, on_change_measure, on_schedule, enabled, visible, alert, alarm_count, created_at, updated_at) VALUES
 (1, 5, 1, 0010001, NULL, NULL, 'EM-1-01-CZ', 'EM-1-01-CZ', NULL, NULL, NULL, NULL, true, true, false, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), 
 (1, 5, 2, 0020001, NULL, NULL, 'EM-2-01-CA', 'EM-2-01-CA', NULL, NULL, NULL, NULL, true, true, false, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
@@ -9,8 +9,8 @@ INSERT INTO conf.element_values (element_type_id, element_type_param_id, param_t
 (1, 4004, 1, 1, NULL, '0x06', true, true, CURRENT_TIMESTAMP,CURRENT_TIMESTAMP), 
 (1, 4005, 1, 1, NULL, '0x20', true, true, CURRENT_TIMESTAMP,CURRENT_TIMESTAMP), 
 
-(1, 4001, 1, 2, NULL, '192.168.88.197', true, true, CURRENT_TIMESTAMP,CURRENT_TIMESTAMP), 
-(1, 4002, 1, 2, NULL, '5000', true, true, CURRENT_TIMESTAMP,CURRENT_TIMESTAMP), 
+(1, 4001, 1, 2, NULL, '172.21.57.82', true, true, CURRENT_TIMESTAMP,CURRENT_TIMESTAMP), 
+(1, 4002, 1, 2, NULL, '4001', true, true, CURRENT_TIMESTAMP,CURRENT_TIMESTAMP), 
 (1, 4003, 1, 2, NULL, '0x05 0x20', true, true, CURRENT_TIMESTAMP,CURRENT_TIMESTAMP), 
 (1, 4004, 1, 2, NULL, '0x06', true, true, CURRENT_TIMESTAMP,CURRENT_TIMESTAMP), 
 (1, 4005, 1, 2, NULL, '0x20', true, true, CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
