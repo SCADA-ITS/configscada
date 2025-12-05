3bc0dfa8a9ae42c75a3b3898cf114c2a0a6e4111
jgarin <joaquin.garin@revenga.com>
2025-12-05 08:23:47 +0100

    #3140

--- ritsfront/resources/db/dml/nogales/03_vo_extensions/element_type/02.entities_values.sql
+++ ritsfront/resources/db/dml/nogales/03_vo_extensions/element_type/02.entities_values.sql
@@ -3,23 +3,23 @@ INSERT INTO ui.vo_entity_values(vo_entity_id, vo_entity_field_id, value) VALUES
 ('ElementType:2', 2, 'element_type_02.svg'),
 ('ElementType:2', 14, 'grid/element_type_02.png'),
 
---('ElementType:3', 1, '3'),
---('ElementType:3', 2, 'graphic_icon/03_detector.svg'),
---('ElementType:3', 14, 'grid/element_type_03.png'),
---('ElementType:3', 3, '10'),
---('ElementType:3', 4, '10'),
---
---('ElementType:4', 1, '4'),
---('ElementType:4', 2, 'graphic_icon/04_section.svg'),
---('ElementType:4', 14, 'grid/element_type_04.png'),
+('ElementType:3', 1, '3'),
+('ElementType:3', 2, 'graphic_icon/03_detector.svg'),
+('ElementType:3', 14, 'grid/element_type_03.png'),
+('ElementType:3', 3, '10'),
+('ElementType:3', 4, '10'),
+
+('ElementType:4', 1, '4'),
+('ElementType:4', 2, 'graphic_icon/04_section.svg'),
+('ElementType:4', 14, 'grid/element_type_04.png'),
 
 ('ElementType:11', 1, '11'),
 ('ElementType:11', 2, 'element_type_11.svg'),
 ('ElementType:11', 14, 'grid/element_type_11.png'),
 
---('ElementType:54', 1, '54'),
---('ElementType:54', 2, 'graphic_icon/51_etd.svg'),
---('ElementType:54', 14, 'grid/element_type_51.png'),
+('ElementType:54', 1, '54'),
+('ElementType:54', 2, 'graphic_icon/51_etd.svg'),
+('ElementType:54', 14, 'grid/element_type_51.png'),
 
 
 ('ElementType:71', 1, '71'),
