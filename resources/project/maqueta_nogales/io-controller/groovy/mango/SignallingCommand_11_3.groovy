96df938551cc8a8676e5cff90dac1fb5695c6ebc
jcaballeroa <jcaballeroa@revenga.com>
2026-01-19 14:41:20 +0100

    Feature #3065

---
+++ resources/project/maqueta_nogales/io-controller/groovy/mango/SignallingCommand_11_3.groovy
@@ -0,0 +1,63 @@
+import java.util.ArrayList
+import java.util.List
+
+import org.apache.commons.lang3.exception.ExceptionUtils
+
+import com.fasterxml.jackson.annotation.JsonInclude.Include
+import com.fasterxml.jackson.core.JsonProcessingException
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.revenga.rits.back.data.core.model.command.SignallingCommand
+import com.revenga.rits.back.io.controller.driver.mango.MangoDriver
+import com.revenga.rits.back.mango.core.model.XidPointValueTimeModel
+import com.revenga.rits.back.mango.core.model.XidPointValueTimeModel.DataTypeEnum
+import com.revenga.rits.back.io.controller.service.EntitiesManager
+
+import com.revenga.rits.back.data.core.util.ResourcesUtil
+
+/**
+ *
+ * SignallingCommand_11_3: SOS volumen micro signalling command
+ *
+ */
+class SignallingCommand_11_3 {
+
+    GroovyShell shell;
+    def signallingCommandUtils;
+
+    static final String VOL_MICRO = 'vol_mic_writer'
+
+
+    org.apache.logging.log4j.Logger log;
+
+    SignallingCommand_11_3(org.apache.logging.log4j.Logger log) {
+        shell = new GroovyShell()
+        signallingCommandUtils = shell.parse(new File(ResourcesUtil.getPath('io-controller/groovy/mango/SignallingCommandUtils.groovy')))
+        this.log = log
+    }
+
+    boolean process(String dataSourceXid, SignallingCommand signallingCommand, MangoDriver driver) {
+        try {
+            List<XidPointValueTimeModel> xidPointValueTimeModels = new ArrayList<>()
+            Long vol_micro_writer_value = Integer.parseInt(signallingCommand.getSignallingParams().get(0).getValue());
+
+            XidPointValueTimeModel xidPointValueTimeModel = signallingCommandUtils.getXidPointValueTimeModel(signallingCommand,
+                    dataSourceXid + '_' + VOL_MICRO, vol_micro_writer_value)
+
+            xidPointValueTimeModels.add(xidPointValueTimeModel)
+
+            ObjectMapper objectMapper = new ObjectMapper()
+            objectMapper.setSerializationInclusion(Include.NON_NULL)
+            String message = objectMapper.writeValueAsString(xidPointValueTimeModels)
+
+            if (driver != null) {
+                driver.send(message)
+            }
+        } catch (NumberFormatException | JsonProcessingException e) {
+            log.error(e.getMessage())
+            log.debug(ExceptionUtils.getStackTrace(e))
+        }
+
+        return true
+    }
+
+}
