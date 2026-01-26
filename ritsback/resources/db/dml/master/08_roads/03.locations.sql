1ecdb0ed9795ef518346b7b81e3dacfefa444dd8
juanpjimenez <juanp.jimenez@revenga.com>
2026-01-26 13:07:18 +0100

    Feature #3168.

--- ritsback/resources/db/dml/master/08_roads/03.locations.sql
+++ ritsback/resources/db/dml/master/08_roads/03.locations.sql
@@ -4,4 +4,5 @@
 INSERT INTO master.location_types (location_type_id, alias, description, label_alias, label_description, enabled, visible, created_at, updated_at) VALUES
 (1, 'Element', 'Location for element', 'LBL_LOCATION_TYPE_ELEMENT', null, true, true, '2020-01-01 00:00:00.000', '2020-01-01 00:00:00.000'),
 (2, 'IncidentReport', 'Location for manual incident report', 'LBL_LOCATION_TYPE_INCIDENT_REPORT', null, true, true, '2020-01-01 00:00:00.000', '2020-01-01 00:00:00.000'),
-(3, 'IncidentReportTemp', 'Temporally location for incident report', 'LBL_LOCATION_TYPE_INCIDENT_REPORT_TEMP', null, true, true, '2020-01-01 00:00:00.000', '2020-01-01 00:00:00.000');
+(3, 'IncidentReportTemp', 'Temporally location for incident report', 'LBL_LOCATION_TYPE_INCIDENT_REPORT_TEMP', null, true, true, '2020-01-01 00:00:00.000', '2020-01-01 00:00:00.000'),
+(4, 'Radar', 'Location for radar', 'LBL_LOCATION_TYPE_RADAR', null, true, true, '2020-01-01 00:00:00.000', '2020-01-01 00:00:00.000');
