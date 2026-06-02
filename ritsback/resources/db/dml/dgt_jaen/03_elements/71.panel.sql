f07512b116e51f53982339fd8916e64c7b2143b7
Jdiaz <jesus.diaz@revenga.com>
2026-06-02 10:36:04 +0200

    #3189

--- ritsback/resources/db/dml/dgt_jaen/03_elements/71.panel.sql
+++ ritsback/resources/db/dml/dgt_jaen/03_elements/71.panel.sql
@@ -558,7 +558,7 @@ INSERT INTO conf.element_values (element_type_id, element_type_param_id, param_t
 (71, 3, 1, 90, NULL, 'DGT', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), 
 (71, 4, 1, 90, NULL, '[{"zone":1,"type":"graphic","vms_group_id":22},{"zone":2,"type":"text","maxCharsPerLine":12},{"zone":3,"type":"graphic","vms_group_id":22}]', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), 
 --(71, 1003, 1, 90, NULL, 'PMV-2-01-CZ', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), 
-(71, 4001, 1, 90, NULL, '172.21.31.100', true, true, CURRENT_TIMESTAMP,CURRENT_TIMESTAMP), 
+(71, 4001, 1, 90, NULL, '172.21.17.51', true, true, CURRENT_TIMESTAMP,CURRENT_TIMESTAMP), 
 (71, 4002, 1, 90, NULL, '4001', true, true, CURRENT_TIMESTAMP,CURRENT_TIMESTAMP), 
 (71, 4003, 1, 90, NULL, '0x05 0x20', true, true, CURRENT_TIMESTAMP,CURRENT_TIMESTAMP), 
 (71, 4004, 1, 90, NULL, '0x06', true, true, CURRENT_TIMESTAMP,CURRENT_TIMESTAMP), 
