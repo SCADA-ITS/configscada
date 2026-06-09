c888dc180ecd8a79793ccd272b758647d171389f
Jdiaz <jesus.diaz@revenga.com>
2026-06-09 15:53:50 +0200

    #3217

---
+++ ritsback/resources/db/dml/dgt_jaen/12_ims/05_type_stretchs/04.iluminacion.sql
@@ -0,0 +1,67 @@
+INSERT INTO conf.ims_incident_type_stretchs (incident_type_id, stretch_id, default_location_id, operational_plan_url, valid_for_min, alias, description, multi_instance, cctv_scenario_id, spath_id, enabled, visible, created_at, updated_at) VALUES
+--SOLEADO
+
+--Despeñaperros
+(401, 1001, 1001, null, null, null, null, false, null, null, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(401, 1002, 1002, null, null, null, null, false, null, null, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+
+--La Cantera
+(401, 2001, 2001, null, null, null, null, false, null, null, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(401, 2002, 2002, null, null, null, null, false, null, null, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+--El corzo
+(401, 3001, 3001, null, null, null, null, false, null, null, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(401, 3002, 3002, null, null, null, null, false, null, null, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+
+--CREPUSCULAR
+
+--Despeñaperros
+(501, 1001, 1001, null, null, null, null, false, null, null, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(501, 1002, 1002, null, null, null, null, false, null, null, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+
+--La Cantera
+(501, 2001, 2001, null, null, null, null, false, null, null, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(501, 2002, 2002, null, null, null, null, false, null, null, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+--El corzo
+(501, 3001, 3001, null, null, null, null, false, null, null, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(501, 3002, 3002, null, null, null, null, false, null, null, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+
+
+--NUBLADO
+
+--Despeñaperros
+(601, 1001, 1001, null, null, null, null, false, null, null, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(601, 1002, 1002, null, null, null, null, false, null, null, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+
+--La Cantera
+(601, 2001, 2001, null, null, null, null, false, null, null, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(601, 2002, 2002, null, null, null, null, false, null, null, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+--El corzo
+(601, 3001, 3001, null, null, null, null, false, null, null, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(601, 3002, 3002, null, null, null, null, false, null, null, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+
+
+--NOCTURNO
+
+--Despeñaperros
+(701, 1001, 1001, null, null, null, null, false, null, null, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(701, 1002, 1002, null, null, null, null, false, null, null, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+
+--La Cantera
+(701, 2001, 2001, null, null, null, null, false, null, null, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(701, 2002, 2002, null, null, null, null, false, null, null, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+--El corzo
+(701, 3001, 3001, null, null, null, null, false, null, null, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(701, 3002, 3002, null, null, null, null, false, null, null, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+
+--EXT/NOCTURNO
+
+--Despeñaperros
+(801, 1001, 1001, null, null, null, null, false, null, null, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(801, 1002, 1002, null, null, null, null, false, null, null, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+
+--La Cantera
+(801, 2001, 2001, null, null, null, null, false, null, null, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(801, 2002, 2002, null, null, null, null, false, null, null, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+--El corzo
+(801, 3001, 3001, null, null, null, null, false, null, null, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(801, 3002, 3002, null, null, null, null, false, null, null, true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
--- ritsfront/resources/db/dml/dgt_jaen/06.vo_extensions/07.notification_levels.sql
+++ ritsfront/resources/db/dml/dgt_jaen/06.vo_extensions/07.notification_levels.sql
@@ -1,5 +1,9 @@
 INSERT INTO ui.vo_entity_values(vo_entity_id, vo_entity_field_id, value) VALUES
 ('ImsIncidentLevel:4', 36, 'true'),
-('ImsIncidentLevel:4', 37, 'incident.mp3');
+('ImsIncidentLevel:4', 37, 'incident.mp3'),
+
+('ImsIncidentLevel:1', 36, 'true');
+
+
 
 
